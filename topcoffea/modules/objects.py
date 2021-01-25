e28b9674a0e03feb213d1e8ad962445f1aecf94d
Kelci Mohrman <kmohrman@earth.crc.nd.edu>
2021-01-25 09:41:27 -0500

    Rework the jet cleaning function

--- topcoffea/modules/objects.py
+++ topcoffea/modules/objects.py
@@ -6,6 +6,7 @@
 
 '''
 import numpy as np
+import awkward as ak
 
 def isTightMuonPOG(pt,eta,dxy,dz,iso,tight_id, tightCharge, year):
     #dxy and dz cuts are baked on tight_id; tight isolation is 0.15
@@ -24,11 +25,14 @@ def isGoodJet(pt, eta, jet_id, jetPtCut=30):
 
 def isClean(jets, electrons, muons, drmin=0.4):
   ''' Returns mask to select clean jets '''
-  epairs = jets.cross(electrons, nested=True)
-  mpairs = jets.cross(muons, nested=True)
-  egoodPairs = (epairs.i0.delta_r(epairs.i1) > drmin).all()
-  mgoodPairs = (mpairs.i0.delta_r(mpairs.i1) > drmin).all()
-  return (egoodPairs) & (mgoodPairs)
+  # Think we can do the jet cleaning like this? It's based on L355 of https://github.com/areinsvo/TTGamma_LongExercise/blob/FullAnalysis/ttgamma/processor.py
+  # However, not really sure how "nearest" works, so probably should look into that more at some point
+  # But this new version of the function does seem to be returning the same mask as the old version
+  jetEle, jetEleDR = jets.nearest(electrons,return_metric=True)
+  jetMu,  jetMuDR  = jets.nearest(muons,return_metric=True)
+  jetEleMask = ak.fill_none(jetEleDR > drmin, True)
+  jetMuMask  = ak.fill_none(jetMuDR > drmin, True)
+  return (jetEleMask & jetMuMask)
 
 def isMuonMVA(pt, eta, dxy, dz, miniIso, sip3D, mvaTTH, mediumPrompt, tightCharge, jetDeepB=0, minpt=15):
   mask = (pt>minpt)&(abs(eta)<2.4)&(abs(dxy)<0.05)&(abs(dz)<0.1)&(miniIso<0.4)&(sip3D<5)&(mvaTTH>0.55)&(mediumPrompt)&(tightCharge==2)&(jetDeepB<0.1522)
