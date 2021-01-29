2b46d8555ec7aa6af9ffd0ce5171097e1453c2d4
Xuan <jrgonzal@cern.ch>
2021-01-29 07:15:45 -0600

    Adding options to run.py

--- analysis/topEFT/run.py
+++ analysis/topEFT/run.py
@@ -17,12 +17,43 @@ from coffea.nanoevents import NanoAODSchema
 import topeft
 from topcoffea.modules import samples
 
-outname = 'plotsTopEFT'
+import argparse
+parser = argparse.ArgumentParser(description='You can customize your run')
+parser.add_argument('cfgfile'           , nargs='?', default=''           , help = 'Config file with dataset names')
+parser.add_argument('--test','-t'       , action='store_true'  , help = 'To perform a test, run over a few events in a couple of chunks')
+parser.add_argument('--nworkers','-n'   , default=8  , help = 'Number of workers')
+parser.add_argument('--chunksize','-s'   , default=500000  , help = 'Number of events per chunk')
+parser.add_argument('--nchunks','-c'   , default=None  , help = 'You can choose to run only a number of chunks')
+parser.add_argument('--outname','-o'   , default='plotsTopEFT', help = 'Name of the output file with histograms')
+parser.add_argument('--outpath','-p'   , default='histos', help = 'Name of the output directory')
+parser.add_argument('--treename'   , default='Events', help = 'Name of the tree inside the files')
 
-nworkers = 8
+args = parser.parse_args()
+cfgfile    = args.cfgfile
+dotest     = args.test
+nworkers   = int(args.nworkers)
+chunksize  = int(args.chunksize)
+nchunks    = int(args.nchunks) if not args.nchunks is None else args.nchunks
+outname    = args.outname
+outpath    = args.outpath
+treename   = args.treename
+
+if dotest:
+  nchunks = 2
+  chunksize = 10000
+  nworkers = 1
+  print('Running a fast test with %i workers, %i chunks of %i events'%(nworkers, nchunks, chunksize))
 
 ### Load samples
-samplesdict = samples.main()
+if cfgfile != '':
+  samplesdict = samples.main()
+elif os.path.isfile('.samples.coffea'):
+  print('Using samples form .samples.coffea')
+  samplesdict = load('.samples.coffea')
+else:
+  print('Execute as [path]/run.py [path]/samples.cfg')
+  exit()
+
 flist = {}; xsec = {}; sow = {}; isData = {}
 for k in samplesdict.keys():
   flist[k] = samplesdict[k]['files']
@@ -34,8 +65,7 @@ processor_instance = topeft.AnalysisProcessor(samplesdict)
 
 # Run the processor and get the output
 tstart = time.time()
-#output = processor.run_uproot_job(flist, treename='Events', processor_instance=processor_instance, executor=processor.futures_executor, executor_args={'workers': nworkers, 'pre_workers': 1}, chunksize=500000)
-output = processor.run_uproot_job(flist, treename='Events', processor_instance=processor_instance, executor=processor.futures_executor, executor_args={"schema": NanoAODSchema,'workers': nworkers, 'pre_workers': 1}, chunksize=500000)
+output = processor.run_uproot_job(flist, treename=treename, processor_instance=processor_instance, executor=processor.futures_executor, executor_args={"schema": NanoAODSchema,'workers': nworkers, 'pre_workers': 1}, chunksize=chunksize, maxchunks=nchunks)
 dt = time.time() - tstart
 
 nbins = sum(sum(arr.size for arr in h._sumw.values()) for h in output.values() if isinstance(h, hist.Hist))
@@ -46,9 +76,10 @@ print("Processing time: %1.2f s with %i workers (%.2f s cpu overall)" % (dt, nwo
 # This is taken from the DM photon analysis...
 # Pickle is not very fast or memory efficient, will be replaced by something better soon
 #    with lz4f.open("pods/"+options.year+"/"+dataset+".pkl.gz", mode="xb", compression_level=5) as fout:
-os.system("mkdir -p histos/")
-print('Saving output in %s...'%("histos/" + outname + ".pkl.gz"))
-with gzip.open("histos/" + outname + ".pkl.gz", "wb") as fout:
+if not outpath.endswith('/'): outpath += '/'
+if not os.path.isdir(outpath): os.system("mkdir -p %s"%outpath)
+print('Saving output in %s...'%(outpath + outname + ".pkl.gz"))
+with gzip.open(outpath + outname + ".pkl.gz", "wb") as fout:
   cloudpickle.dump(output, fout)
 print('Done!')
 
