a87f3ff0a827fd3115549690dfe0f0985a7fa8e4
Kelci Mohrman <kmohrman@earth.crc.nd.edu>
2021-03-03 11:54:05 -0500

    Adding some example jobs as placeholders

---
+++ tests/coffea_check.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Placeholder file for coffea installation stuff
+# Can probably base this file on: https://github.com/cooperative-computing-lab/cctools-packaging-test/blob/main/coffea.sh
+
+unset PYTHONPATH
+
+
---
+++ tests/run_over_testfiles.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+# Placeholder file for testing processor
+# Probably we can base this test on https://github.com/bu-cms/bucoffea/blob/master/test/run_over_testfiles.sh
