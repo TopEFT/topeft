44be7732a30e261ec8fe53d9c51e7671431a5955
Kelci Mohrman <kmohrman@earth.crc.nd.edu>
2021-03-15 20:45:23 -0400

    Use topcoffea_path function to find root files in corrections.py

--- topcoffea/modules/corrections.py
+++ topcoffea/modules/corrections.py
@@ -7,18 +7,12 @@
 import uproot
 from coffea import hist, lookup_tools
 import os, sys
-
-# Should probably do this in a different way...
-basepath = os.path.abspath(__file__).rsplit('/')
-basepath = basepath[:len(basepath)-3]
-basepath = "/"+os.path.join(*basepath)
+from topcoffea.modules.paths import topcoffea_path
 
 def GetHistoFun(fname, hname):
   f = uproot.open(fname)
   h = f[hname]
   return lookup_tools.dense_lookup.dense_lookup(h.values, h.edges)
 
-#getMuonIso = GetHistoFun(basepath+'data/scaleFactors/MuonISO.root', 'NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta')
-#getMuonId  = GetHistoFun(basepath+'data/scaleFactors/MuonID.root',  'NUM_TightID_DEN_genTracks_pt_abseta')
-getMuonIso = GetHistoFun(os.path.join(basepath,'data/scaleFactors/MuonISO.root'), 'NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta')
-getMuonId  = GetHistoFun(os.path.join(basepath,'data/scaleFactors/MuonID.root'), 'NUM_TightID_DEN_genTracks_pt_abseta')
+getMuonIso = GetHistoFun(topcoffea_path('data/scaleFactors/MuonISO.root'), 'NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta')
+getMuonId  = GetHistoFun(topcoffea_path('data/scaleFactors/MuonID.root'), 'NUM_TightID_DEN_genTracks_pt_abseta')
