5ea8fdf826c229e1e890e0b4c39c74bbbf339999
Kevin Lannon <klannon@nd.edu>
2021-05-13 05:54:09 -0400

    Fix problems with bin contents/errors
    There were two problems addressed in this commit:
    1.  The values of non-EFT bins were not properly returned by the  values() method in HistEFT
    2.  When not using the EFT quartic coefficients to parameterize the bin sumw2, the bin contents were returned as bin the bin sumw2 also.  This is the correct behavior for *unweighted* bins, but for weighted bins (which EFT always are) is not desireable, leading to unviewable histograms.  This commit changes the behavior so that values() returns a very, very small value for the bin errors.  It doesn't return zero because if all sumw2 are zero, coffea generates RunTimeWarnings.

--- topcoffea/modules/HistEFT.py
+++ topcoffea/modules/HistEFT.py
@@ -415,13 +415,22 @@ class HistEFT(coffea.hist.Hist):
 
       if is_eft_bin:
         _sumw = self._eft_helper.calc_eft_weights(self._sumw[sparse_key],self._wcs)
+      else:
+        _sumw = self._sumw
 
       if sumw2:
         if self._sumw2 is not None:
-          _sumw2 = self._eft_helper.calc_eft_w2(self._sumw2[sparse_key],self._wcs)
-          w2 = view_dim(_sumw2)
+          if is_eft_bin:
+            _sumw2 = self._eft_helper.calc_eft_w2(self._sumw2[sparse_key],self._wcs)
+          else:
+            _sumw2 = self._sumw2
         else:
-          w2 = view_dim(_sumw)
+          if is_eft_bin:
+            # Set really tiny error bars
+            _sumw2 = 1e-30*np.ones_like(_sumw)
+          else:
+            _sumw2 = _sumw
+        w2 = view_dim(_sumw2)
         out[id_key] = (view_dim(_sumw), w2)
       else:
         out[id_key] = view_dim(_sumw)
