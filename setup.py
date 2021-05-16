0ce1a7887b02a011ece3f952e8c27fa0396bfb0e
Kelci Mohrman <kmohrman@nd.edu>
2021-05-16 19:19:42 -0400

    Specify files from data directory in setup script, otherwise they are not included

--- setup.py
+++ setup.py
@@ -5,8 +5,16 @@ setuptools.setup(
     version='0.0.0',
     description='Top quark analyses using the Coffea framework',
     packages=setuptools.find_packages(),
+    # Include data files (Note: "include_package_data=True" does not seem to work)
     package_data={
-        "topcoffea" : ["cfg/*.cfg","data/scaleFactors/*.root"],
+        "topcoffea" : [
+            "cfg/*.cfg",
+            "data/scaleFactors/*.root",
+            "data/fromTTH/fakerate/*.root",
+            "data/fromTTH/fliprates/*.root",
+            "data/fromTTH/lepSF/*/*/*.root",
+            "data/fromTTH/lepSF/*/*/*/*.root",
+        ],
     }
 )
 
