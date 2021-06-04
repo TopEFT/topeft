549b59037a775224b0286702949a99a144f5a33c
Kelci Mohrman <kmohrman@nd.edu>
2021-06-03 20:12:35 -0400

    Adding private UL18 files

--- analysis/topEFT/make_jsons_for_private_UL.py
+++ analysis/topEFT/make_jsons_for_private_UL.py
@@ -36,27 +36,27 @@ private_UL17_dict = {
 
 private_UL18_dict = {
 
-    "UL18_ttHJet_b1"   : "",
-    "UL18_ttHJet_b2"   : "",
-    "UL18_ttHJet_b3"   : "",
+    "UL18_ttHJet_b1"   : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch1/naodOnly_step/v4/nAOD_step_ttHJet_all22WCsStartPtCheckdim6TopMay20GST_run0",
+    "UL18_ttHJet_b2"   : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch2/naodOnly_step/v1/nAOD_step_ttHJet_all22WCsStartPtCheckdim6TopMay20GST_run0",
+    "UL18_ttHJet_b3"   : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch3/naodOnly_step/v1/nAOD_step_ttHJet_all22WCsStartPtCheckdim6TopMay20GST_run0",
 
-    "UL18_ttlnuJet_b1" : "",
-    "UL18_ttlnuJet_b2" : "",
-    "UL18_ttlnuJet_b3" : "",
+    "UL18_ttlnuJet_b1" : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch1/naodOnly_step/v4/nAOD_step_ttlnuJet_all22WCsStartPtCheckdim6TopMay20GST_run0",
+    "UL18_ttlnuJet_b2" : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch2/naodOnly_step/v1/nAOD_step_ttlnuJet_all22WCsStartPtCheckdim6TopMay20GST_run0",
+    "UL18_ttlnuJet_b3" : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch3/naodOnly_step/v1/nAOD_step_ttlnuJet_all22WCsStartPtCheckdim6TopMay20GST_run0",
 
-    "UL18_ttllJet_b1"  : "",
-    "UL18_ttllJet_b2"  : "",
-    "UL18_ttllJet_b3"  : "",
+    "UL18_ttllJet_b1"  : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch1/naodOnly_step/v4/nAOD_step_ttllNuNuJetNoHiggs_all22WCsStartPtCheckdim6TopMay20GST_run0",
+    "UL18_ttllJet_b2"  : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch2/naodOnly_step/v1/nAOD_step_ttllNuNuJetNoHiggs_all22WCsStartPtCheckdim6TopMay20GST_run0",
+    "UL18_ttllJet_b3"  : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch3/naodOnly_step/v1/nAOD_step_ttllNuNuJetNoHiggs_all22WCsStartPtCheckdim6TopMay20GST_run0",
 
-    "UL18_tllq_b1"     : "",
-    "UL18_tllq_b2"     : "",
-    "UL18_tllq_b3"     : "",
+    "UL18_tllq_b1"     : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch1/naodOnly_step/v4/nAOD_step_tllq4fNoSchanWNoHiggs0p_all22WCsStartPtCheckV2dim6TopMay20GST_run0",
+    "UL18_tllq_b2"     : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch2/naodOnly_step/v1/nAOD_step_tllq4fNoSchanWNoHiggs0p_all22WCsStartPtCheckV2dim6TopMay20GST_run0",
+    "UL18_tllq_b3"     : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch3/naodOnly_step/v1/nAOD_step_tllq4fNoSchanWNoHiggs0p_all22WCsStartPtCheckV2dim6TopMay20GST_run0",
 
-    "UL18_tHq_b1"      : "",
-    "UL18_tHq_b2"      : "",
-    "UL18_tHq_b3"      : "",
+    "UL18_tHq_b1"      : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch1/naodOnly_step/v4/nAOD_step_tHq4f_all22WCsStartPtCheckdim6TopMay20GST_run0",
+    "UL18_tHq_b2"      : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch2/naodOnly_step/v1/nAOD_step_tHq4f_all22WCsStartPtCheckdim6TopMay20GST_run0",
+    "UL18_tHq_b3"      : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch3/naodOnly_step/v1/nAOD_step_tHq4f_all22WCsStartPtCheckdim6TopMay20GST_run0",
 
-    "UL18_tttt_b4"     : "",
+    "UL18_tttt_b4"     : "/store/user/kmohrman/FullProduction/FullR2/UL18/Round1/Batch4/naodOnly_step/v1/nAOD_step_tttt_FourtopsMay3v1_run0",
 }
 
 private_UL16_dict = {
@@ -79,6 +79,7 @@ private_UL16APV_dict = {
     "UL16APV_tttt_b1"     : "",
 }
 
+# Maps a sample name to a name in xsec.cfg
 def find_xsec_name(sample_name):
     name_map = {
         "ttH"   : "ttHnobb",
@@ -93,6 +94,7 @@ def find_xsec_name(sample_name):
             xsec_name_match = xsec_name
             return xsec_name
 
+# Wrapper for createJSON.py
 def make_json(sample_dir,sample_name,prefix,sample_yr,xsec_name):
     subprocess.run([
         "python",
@@ -105,6 +107,13 @@ def make_json(sample_dir,sample_name,prefix,sample_yr,xsec_name):
         "--year"      , sample_yr,
     ])
 
+# Convenience function for running make_json() on all entries in a dictionary of samples, and moving the results to out_dir
+def make_jsons_for_dict_of_samples(samples_dict,prefix,year,out_dir):
+    for sname,sdir in samples_dict.items():
+        make_json(sdir,sname,prefix,year,find_xsec_name(sname))
+        out_name = sname+".json"
+        subprocess.run(["mv",out_name,out_dir]) 
+        print("sname, sdir, out_name:",sname,sdir,out_name)
 
 def main():
 
@@ -115,11 +124,15 @@ def main():
     #xsec_name = "TTTo2L2Nu"
     #make_json(sample_dir,sample_name,sample_yr,xsec_name)
 
+    #for sname,sdir in private_UL17_dict.items():
+    #    make_json(sdir,sname,"/hadoop","2017",find_xsec_name(sname)) # Takes about 30 min
+    #    out_name = sname+".json"
+    #    subprocess.run(["mv",out_name,out_dir]) 
+
     out_dir = "../../topcoffea/json/signal_samples/"
 
-    for sname,sdir in private_UL17_dict.items():
-        make_json(sdir,sname,"/hadoop","2017",find_xsec_name(sname)) # Takes about 30 min
-        out_name = sname+".json"
-        subprocess.run(["mv",out_name,out_dir]) 
+    #make_jsons_for_dict_of_samples(private_UL17_dict,"/hadoop","2017",out_dir)
+    make_jsons_for_dict_of_samples(private_UL18_dict,"/hadoop","2018",out_dir)
+
 
 main()
