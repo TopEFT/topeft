0286ee1fb0c1295ffa07db5267f4768308c03f8b
Kelci Mohrman <kmohrman@nd.edu>
2021-07-11 22:56:23 -0400

    Adding placeholder config file for the yield check to use

--- analysis/topEFT/check_yields.sh
+++ analysis/topEFT/check_yields.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+
 # What we want to call the output files
 FILE_NAME="check_yields"
 
 # The json we want to compare against
 REF_FILE_NAME="test/placeholder_ref_yields.json"
 
+
 # Run the processor
 printf "\nRunning processor...\n"
-time python run.py ../../topcoffea/cfg/comp_sample.cfg -o $FILE_NAME
+time python run.py ../../topcoffea/cfg/check_yields_sample.cfg -o $FILE_NAME
 
 # Make the jsons
 printf "\nMaking yields json from pkl...\n"
 python get_yield_json.py histos/$FILE_NAME.pkl.gz -n $FILE_NAME --quiet
+
+# If we want this to be the new ref json (do we want to have an option for this?):
+#cp $FILE_NAME.json tests/$REF_FILE_NAME
 
 # Compare the yields to the ref json
 printf "\nComparing yields agains reference...\n"
