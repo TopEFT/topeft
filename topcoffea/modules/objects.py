93ec11f8b636543c636acd93b78f81bc41a13819
Kelci Mohrman <kmohrman@nd.edu>
2021-07-31 06:04:46 -0400

    Updates to handle years as strings

--- topcoffea/modules/objects.py
+++ topcoffea/modules/objects.py
@@ -22,18 +22,32 @@ def ttH_idEmu_cuts_E3(hoe, eta, deltaEtaSC, eInvMinusPInv, sieie):
   return (hoe<(0.10-0.00*(abs(eta+deltaEtaSC)>1.479))) & (eInvMinusPInv>-0.04) & (sieie<(0.011+0.019*(abs(eta+deltaEtaSC)>1.479)))
 
 def smoothBFlav(jetpt,ptmin,ptmax,year,scale_loose=1.0):
-  wploose = (0.0614, 0.0521, 0.0494)
-  wpmedium = (0.3093, 0.3033, 0.2770)
+
+  # Get the btag wp for the year
+  if ((year == "2016") or (year == "2016APV")):
+    wploose  = 0.0614
+    wpmedium = 0.3093
+  elif (year == "2017"):
+    wploose  = 0.0521
+    wpmedium = 0.3033
+  elif (year == "2018"):
+    wploose  = 0.0494
+    wpmedium = 0.2770
+  else:
+    raise Exception(f"Error: Unknown year \"{year}\". Exiting...")
+
   x = np.minimum(np.maximum(0, jetpt - ptmin)/(ptmax-ptmin), 1.0)
-  return x*wploose[year-2016]*scale_loose + (1-x)*wpmedium[year-2016]
+  return x*wploose*scale_loose + (1-x)*wpmedium
 
 def coneptElec(pt, mvaTTH, jetRelIso):
   conePt = (0.90 * pt * (1 + jetRelIso))
   return ak.where((mvaTTH>0.80),pt,conePt)
+  #return ak.where((mvaTTH>0.75),pt,conePt)
 
 def coneptMuon(pt, mvaTTH, jetRelIso, mediumId):
   conePt = (0.90 * pt * (1 + jetRelIso))
   return ak.where(((mvaTTH>0.85)&(mediumId>0)),pt,conePt)
+  #return ak.where(((mvaTTH>0.95)&(mediumId>0)),pt,conePt)
 
 def isPresElec(pt, eta, dxy, dz, miniIso, sip3D, eleId):
   mask = (pt>7)&(abs(eta)<2.5)&(abs(dxy)<0.05)&(abs(dz)<0.1)&(miniIso<0.4)&(sip3D<8)&(eleId)
@@ -50,25 +64,49 @@ def isLooseMuon(miniPFRelIso_all,sip3d,looseId):
   return (miniPFRelIso_all<0.4) & (sip3d<8) & (looseId)
 
 def isFOElec(conept, jetBTagDeepFlav, ttH_idEmu_cuts_E3, convVeto, lostHits, mvaTTH, jetRelIso, mvaFall17V2noIso_WP80, year):
-  bTagCut = 0.3093 if year==2016 else 0.3033 if year==2017 else 0.2770
+
+  # Get the btag cut for the year
+  if ((year == "2016") or (year == "2016APV")):
+    bTagCut = 0.3093
+  elif (year == "2017"):
+    bTagCut = 0.3033
+  elif (year == "2018"):
+    bTagCut = 0.2770
+  else:
+    raise Exception(f"Error: Unknown year \"{year}\". Exiting...")
+
   btabReq    = (jetBTagDeepFlav<bTagCut)
   ptReq      = (conept>10)
   qualityReq = (ttH_idEmu_cuts_E3 & convVeto & (lostHits==0))
   mvaReq     = ((mvaTTH>0.80) | ((mvaFall17V2noIso_WP80) & (jetRelIso<0.70)))
+  #mvaReq     = ((mvaTTH>0.75) | ((mvaFall17V2noIso_WP80) & (jetRelIso<0.70)))
   return ptReq & btabReq & qualityReq & mvaReq
 
 def isFOMuon(pt, conept, jetBTagDeepFlav, mvaTTH, jetRelIso, year):
-  bTagCut = 0.3093 if year==2016 else 0.3033 if year==2017 else 0.2770
+
+  # Get the btag cut for the year
+  if ((year == "2016") or (year == "2016APV")):
+    bTagCut = 0.3093
+  elif (year == "2017"):
+    bTagCut = 0.3033
+  elif (year == "2018"):
+    bTagCut = 0.2770
+  else:
+    raise Exception(f"Error: Unknown year \"{year}\". Exiting...")
+
   btagReq = (jetBTagDeepFlav<bTagCut)
   ptReq   = (conept>10)
   mvaReq  = ((mvaTTH>0.85) | ((jetBTagDeepFlav<smoothBFlav(0.9*pt*(1+jetRelIso),20,45,year)) & (jetRelIso < 0.50)))
+  #mvaReq  = ((mvaTTH>0.95) | ((jetBTagDeepFlav<smoothBFlav(0.9*pt*(1+jetRelIso),20,45,year)) & (jetRelIso < 0.50)))
   return ptReq & btagReq & mvaReq
 
 def tightSelElec(clean_and_FO_selection_TTH, mvaTTH):
   return (clean_and_FO_selection_TTH) & (mvaTTH > 0.80)
+  #return (clean_and_FO_selection_TTH) & (mvaTTH > 0.75)
 
 def tightSelMuon(clean_and_FO_selection_TTH, mediumId, mvaTTH):
   return (clean_and_FO_selection_TTH) & (mediumId>0) & (mvaTTH > 0.85)
+  #return (clean_and_FO_selection_TTH) & (mediumId>0) & (mvaTTH > 0.95)
 
 def isClean(obj_A, obj_B, drmin=0.4):
    objB_near, objB_DR = obj_A.nearest(obj_B, return_metric=True)
