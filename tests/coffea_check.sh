07a6216a9fe0a0c6f37142d5271a37e7186ff661
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-08-18 15:16:28 -0400

    remove placeholder files

--- tests/coffea_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Placeholder file for coffea installation stuff
-# Can probably base this file on: https://github.com/cooperative-computing-lab/cctools-packaging-test/blob/main/coffea.sh
-
-unset PYTHONPATH
-
-
--- tests/run_over_testfiles.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-# Placeholder file for testing processor
-# Probably we can base this test on https://github.com/bu-cms/bucoffea/blob/master/test/run_over_testfiles.sh
