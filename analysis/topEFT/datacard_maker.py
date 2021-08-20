dd54b319ef5ae07035e32edccf0e3d36ec62e78e
Brent Yates <yates.313@osu.edu>
2021-08-20 12:44:54 -0400

    Updated rebinning in datacard maker

--- analysis/topEFT/datacard_maker.py
+++ analysis/topEFT/datacard_maker.py
@@ -179,9 +179,9 @@ class DatacardMaker():
                 elif '3l' in channel: h_base = h_base.rebin('njets', hist.Bin("njets",  "Jet multiplicity ", [2,3,4,5]))
                 elif '4l' in channel: h_base = h_base.rebin('njets', hist.Bin("njets",  "Jet multiplicity ", [2,3,4]))
             if 'ht' in variable:
-                h_base = h_base.rebin('ht', hist.Bin("ht", "H$_{T}$ (GeV)", 10, 0, 1000))
+                h_base = h_base.rebin('ht', hist.Bin("ht", "H$_{T}$ (GeV)", 10, 0, 2000))
             if 'ptbl' in variable:
-                h_base = h_base.rebin('ptbl', hist.Bin("ptbl", "$p_{T}^{b\mathrm{-}jet+\ell_{min(dR)}}$", 10, 0, 500))
+                h_base = h_base.rebin('ptbl', hist.Bin("ptbl", "$p_{T}^{b\mathrm{-}jet+\ell_{min(dR)}}$", 10, 0, 1000))
             #Save the SM plot
             h_sm = h_base
             h_sm.set_sm()
