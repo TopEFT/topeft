c8fb1e3830f4cb071b88e6838f201914e8df5599
Kelci Mohrman <kmohrman@nd.edu>
2021-09-11 19:29:00 -0400

    When comparing yield dictionaries, do not crash if keys are different

--- topcoffea/modules/YieldTools.py
+++ topcoffea/modules/YieldTools.py
@@ -136,6 +136,27 @@ class YieldTools():
             p = (float(a)-float(b))/float(b)
         return p
 
+    # Takes two dictionaries, returns the list of lists [common keys, keys unique to d1, keys unique to d2]
+    def get_common_keys(self,dict1,dict2):
+
+        common_lst = []
+        unique_1_lst = []
+        unique_2_lst = []
+
+        # Find common keys, and keys unique to d1
+        for k1 in dict1.keys():
+            if k1 in dict2.keys():
+                common_lst.append(k1)
+            else:
+                unique_1_lst.append(k1)
+
+        # Find keys unique to d2
+        for k2 in dict2.keys():
+            if k2 not in common_lst:
+                unique_2_lst.append(k2)
+
+        return [common_lst,unique_1_lst,unique_2_lst]
+
 
     # Get the per lepton e/m factor from e.g. eee and mmm yields
     def get_em_factor(self,e_val,m_val,nlep):
@@ -186,6 +207,7 @@ class YieldTools():
         h = h.rebin('njets', hist.Bin("njets",  "Jet multiplicity ", [bin_val,bin_val+1]))
         return h
 
+            
 
     # Get the difference between values in nested dictionary, currently can get either percent diff, or absolute diff
     # Returns a dictionary in the same format (currently does not propagate errors, just returns None)
@@ -196,27 +218,32 @@ class YieldTools():
     #   }
     def get_diff_between_nested_dicts(self,dict1,dict2,difftype):
 
+        # Get list of keys common to both dictionaries
+        common_keys, d1_keys, d2_keys = self.get_common_keys(dict1,dict2)
+        if len(d1_keys+d2_keys) > 0:
+            print(f"\nWARNING, keys {d1_keys+d2_keys} are not in both dictionaries.")
+
         ret_dict = {}
-        for k1 in dict1.keys():
+        for k in common_keys:
 
-            if k1 in dict2.keys():
-                ret_dict[k1] = {}
-                for subk1 in dict1[k1].keys():
-                    if subk1 not in dict2[k1].keys():
-                        raise Exception("These dictionaries do not have the same structure. Exiting...")
-                    v1,e1 = dict1[k1][subk1]
-                    v2,e1 = dict2[k1][subk1]
-                    if difftype == "percent_diff":
-                        ret_diff = self.get_pdiff(v1,v2)
-                    elif difftype == "absolute_diff":
-                        ret_diff = v1 - v2
-                    else:
-                        raise Exception(f"Unknown diff type: {difftype}. Exiting...")
+            ret_dict[k] = {}
 
-                    ret_dict[k1][subk1] = (ret_diff,None)
-            else:
-                print(f"WARNING, key {k1} is not in both dictionaries. Continuing...")
-                continue
+            # Get list of sub keys common to both sub dictionaries
+            common_subkeys, d1_subkeys, d2_subkeys = self.get_common_keys(dict1[k],dict2[k])
+            if len(d1_subkeys+d2_subkeys) > 0:
+                print(f"\tWARNING, sub keys {d1_subkeys+d2_subkeys} are not in both dictionaries.")
+
+            for subk in common_subkeys:
+                v1,e1 = dict1[k][subk]
+                v2,e1 = dict2[k][subk]
+                if difftype == "percent_diff":
+                    ret_diff = self.get_pdiff(v1,v2)
+                elif difftype == "absolute_diff":
+                    ret_diff = v1 - v2
+                else:
+                    raise Exception(f"Unknown diff type: {difftype}. Exiting...")
+
+                ret_dict[k][subk] = (ret_diff,None)
 
         return ret_dict
 
