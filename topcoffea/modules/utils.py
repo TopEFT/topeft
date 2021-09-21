8b648c86bee58614bf649600b1dea6129e4c889d
Andrew42 <awightma@nd.edu>
2021-09-21 13:49:49 -0500

    Minor optimization to regex function

--- topcoffea/modules/utils.py
+++ topcoffea/modules/utils.py
@@ -6,11 +6,12 @@ def regex_match(lst,regex_lst):
     #       This means that any regex special characters in the regex_lst should be properly
     #       escaped prior to calling this function.
     # NOTE: The input list is assumed to be a list of str objects and nothing else!
-    matches = []
     if len(regex_lst) == 0: return lst[:]
+    matches = []
+    to_match = [re.compile(r"{}".format(x)) for x in regex_lst]
     for s in lst:
-        for pat in regex_lst:
-            m = re.search(r"{}".format(pat),s)
+        for pat in to_match:
+            m = pat.search(s)
             if m is not None:
                 matches.append(s)
                 break
