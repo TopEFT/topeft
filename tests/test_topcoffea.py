86b338b02418e1c3a1ffc028b80cf6eb08bffdf9
Brent Yates <yates.313@osu.edu>
2021-10-05 13:29:05 -0400

    Fixed typo in test file

--- tests/test_topcoffea.py
+++ tests/test_topcoffea.py
@@ -8,7 +8,7 @@ def test_topcoffea():
         "analysis/topEFT/run.py",
         "topcoffea/json/test_samples/UL17_private_ttH_for_CI.json",
         "-o",
-        "output_check_yields"
+        "output_check_yields",
         "-p",
         "analysis/topEFT/histos/"
     ]
