e808fc2a52ecfb85e87eb66e2fa2df7c7951910b
Brent Yates <yates.313@osu.edu>
2021-11-09 21:48:28 -0500

    Improved script

--- analysis/topEFT/remake_ci_ref_datacard.sh
+++ analysis/topEFT/remake_ci_ref_datacard.sh
@@ -2,18 +2,24 @@
 # Run this script when you want to update the reference file
 # Make sure you've run pytest already, to generate the files to copy
 
-echo -e "Running initial scans\nIgnore any errors.\n"
 if [[ "$PWD" == *"analysis/topEFT"* ]]; then
     cwd=$PWD
     cd ../../
 fi
-python analysis/topEFT/remake_ci_ref_datacard.py
+if [[ ! -f "analysis/topEFT/histos/output_check_yields_nonprompt.pkl.gz" ]]; then
+echo -e "Running TopCoffea first.\nMake sure you have the nanoAOD test file.\n"
+python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_topcoffea()'
+python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_nonprompt()'
+fi
+echo -e "Running initial scans\nIgnore any errors.\n"
+mkdir -p histos
+python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_datacard_2l_nuis()'
 cp histos/ttx_multileptons-2lss_p_2b.txt analysis/topEFT/test/ttx_multileptons-2lss_p_2b_ref.txt
-python analysis/topEFT/remake_ci_ref_datacard.py
+python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_datacard_2l_ht()'
 cp histos/ttx_multileptons-2lss_p_4j_2b_ht.txt analysis/topEFT/test/ttx_multileptons-2lss_p_4j_2b_ht_ref.txt
-python analysis/topEFT/remake_ci_ref_datacard.py
+python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_datacard_3l()'
 cp histos/ttx_multileptons-3l_sfz_1b.txt analysis/topEFT/test/ttx_multileptons-3l_sfz_1b_ref.txt
-python analysis/topEFT/remake_ci_ref_datacard.py
+python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_datacard_3l_ptbl()'
 cp histos/ttx_multileptons-3l_onZ_1b_2j_ptbl.txt analysis/topEFT/test/ttx_multileptons-3l_onZ_1b_2j_ptbl_ref.txt
 echo -e "\nRunning onces more to make sure everyting worked\nErrors ARE important!\n"
 python analysis/topEFT/remake_ci_ref_datacard.py
