dd166b30169dfcd0bfada5fb5cd3a84ac9ecfe87
Brent Yates <yates.313@osu.edu>
2021-11-11 10:48:19 -0500

    Updated json files

--- analysis/topEFT/make_jsons.py
+++ analysis/topEFT/make_jsons.py
@@ -501,7 +501,7 @@ central_UL17_bkg_dict = {
         "xsecName": "ST_antitop_t-channel",
     },
     "UL17_tbarW" : {
-        "path" : "/ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM" 
+        "path" : "/ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM",
         "histAxisName": "tbarW_centralUL17",
         "xsecName": "ST_tW_antitop_5f_inclusiveDecays",
     },
