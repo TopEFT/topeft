0502eda18bed0d2a0efe16714af31ca2f8b4a1bc
Yuyi Wan <ywan2@earth.crc.nd.edu>
2022-01-27 13:08:30 -0500

    test original topcoffea.py

--- tests/test_topcoffea.py
+++ tests/test_topcoffea.py
@@ -6,6 +6,24 @@ from os.path import exists
 from os import getcwd
 
 
+def test_topcoffea():
+    args = [
+        "time",
+        "python",
+        "analysis/topEFT/run.py",
+        "topcoffea/json/test_samples/UL17_private_ttH_for_CI.json",
+        "-o",
+        "output_check_yields",
+        "-p",
+        "analysis/topEFT/histos/"
+    ]
+
+    # Run TopCoffea
+    subprocess.run(args)
+
+    assert(exists('analysis/topEFT/histos/output_check_yields.pkl.gz'))
+
+
 def test_topcoffea_wq():
     port=9123
     factory = Factory("local", manager_host_port="localhost:{}".format(port))
