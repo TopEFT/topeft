df55c3a85a6cc66a9c5f82de05a16862b1700767
Brent Yates <yates.313@osu.edu>
2022-02-02 21:08:23 -0500

    Fixed `remake_ci_ref_datacard` after name change

--- analysis/topEFT/remake_ci_ref_datacard.sh
+++ analysis/topEFT/remake_ci_ref_datacard.sh
@@ -8,18 +8,18 @@ fi
 if [[ ! -f "analysis/topEFT/histos/output_check_yields_nonprompt.pkl.gz" ]]; then
 echo -e "Running TopCoffea first.\nMake sure you have the nanoAOD test file.\n"
 wget -nc http://www.crc.nd.edu/~kmohrman/files/root_files/for_ci/ttHJet_UL17_R1B14_NAOD-00000_10194.root
-python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_topcoffea()'
-python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_nonprompt()'
+python -c 'import tests.test_futures; tests.test_futures.test_topcoffea()'
+python -c 'import tests.test_futures; tests.test_futures.test_nonprompt()'
 fi
 echo -e "Running initial scans\nIgnore any errors.\n"
 mkdir -p histos
-python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_datacard_2l()'
+python -c 'import tests.test_futures; tests.test_futures.test_datacard_2l()'
 cp histos/ttx_multileptons-2lss_p_2b.txt analysis/topEFT/test/ttx_multileptons-2lss_p_2b_ref.txt
-python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_datacard_2l_ht()'
+python -c 'import tests.test_futures; tests.test_futures.test_datacard_2l_ht()'
 cp histos/ttx_multileptons-2lss_p_4j_2b_ht.txt analysis/topEFT/test/ttx_multileptons-2lss_p_4j_2b_ht_ref.txt
-python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_datacard_3l()'
+python -c 'import tests.test_futures; tests.test_futures.test_datacard_3l()'
 cp histos/ttx_multileptons-3l_sfz_1b.txt analysis/topEFT/test/ttx_multileptons-3l_sfz_1b_ref.txt
-python -c 'import tests.test_topcoffea; tests.test_topcoffea.test_datacard_3l_ptbl()'
+python -c 'import tests.test_futures; tests.test_futures.test_datacard_3l_ptbl()'
 cp histos/ttx_multileptons-3l_onZ_1b_2j_ptbl.txt analysis/topEFT/test/ttx_multileptons-3l_onZ_1b_2j_ptbl_ref.txt
 echo -e "\nRunning once more to make sure everyting worked\nErrors ARE important!\n"
 python analysis/topEFT/remake_ci_ref_datacard.py
