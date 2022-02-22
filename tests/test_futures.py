e27dc464aab321afbf82675befde1724c664955a
Brent Yates <yates.313@osu.edu>
2022-02-22 10:25:41 -0500

    Moved yields checks into pytest

--- tests/test_futures.py
+++ tests/test_futures.py
@@ -29,36 +29,6 @@ def test_nonprompt():
 
     assert(exists('analysis/topEFT/histos/output_check_yields_nonprompt.pkl.gz'))
 
-def test_make_yields():
-    args = [
-        "python",
-        "analysis/topEFT/get_yield_json.py",
-        "-f",
-        "analysis/topEFT/histos/output_check_yields.pkl.gz",
-        "-n",
-        "analysis/topEFT/output_check_yields"
-    ]
-
-    # Produce json
-    subprocess.run(args)
-    assert(exists('analysis/topEFT/output_check_yields.json'))
-
-def test_compare_yields():
-    args = [
-        "python",
-        "analysis/topEFT/comp_yields.py",
-        "analysis/topEFT/output_check_yields.json",
-        "analysis/topEFT/test/UL17_private_ttH_for_CI_yields.json",
-        "-t1",
-        "New yields",
-        "-t2",
-        "Ref yields"
-    ]
-
-    # Run comparison
-    out = subprocess.run(args, stdout=True)
-    assert(out.returncode == 0) # Returns 0 if all pass
-
 def test_datacard_2l():
     args = [
         "python",
