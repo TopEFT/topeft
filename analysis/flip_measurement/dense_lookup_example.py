a39925541c467bdff4df2898bfe0614e7cdc4532
Kelci Mohrman <kmohrman@nd.edu>
2022-04-17 23:04:55 -0400

    Clean up a bit

--- analysis/flip_measurement/dense_lookup_example.py
+++ analysis/flip_measurement/dense_lookup_example.py
@@ -2,39 +2,51 @@ import gzip
 import pickle
 from coffea import lookup_tools
 
-pkl_lst = [
+# The committed pkl files
+pkl_lst_committed = [
     "../../topcoffea/data/fliprates/flip_probs_topcoffea_UL16APV.pkl.gz",
     "../../topcoffea/data/fliprates/flip_probs_topcoffea_UL16.pkl.gz",
     "../../topcoffea/data/fliprates/flip_probs_topcoffea_UL17.pkl.gz",
     "../../topcoffea/data/fliprates/flip_probs_topcoffea_UL18.pkl.gz",
 ]
 
-# Before scaling
-pkl_lst_old = [
-    "../../topcoffea/data/fromTTH/fliprates/flip_probs_apr06BinningAN19127_UL16APV.pkl.gz",
-    "../../topcoffea/data/fromTTH/fliprates/flip_probs_apr06BinningAN19127_UL16.pkl.gz",
-    "../../topcoffea/data/fromTTH/fliprates/flip_probs_apr06BinningAN19127_UL17.pkl.gz",
-    "../../topcoffea/data/fromTTH/fliprates/flip_probs_apr06BinningAN19127_UL18.pkl.gz",
+# Local pkl files
+pkl_lst_local = [
+    "flip_probs_topcoffea_UL16APV.pkl.gz",
+    "flip_probs_topcoffea_UL16.pkl.gz",
+    "flip_probs_topcoffea_UL17.pkl.gz",
+    "flip_probs_topcoffea_UL18.pkl.gz",
 ]
 
-# Dump the values currently in the committed fliprates pkl files
-for pkl_file in pkl_lst:
-    print("Looking at:",pkl_file)
-    with gzip.open(pkl_file) as fin:
+# Print values in pkl files
+def dump_vals(pkl_filepath_lst):
+    for pkl_file in pkl_filepath_lst:
+        print("Looking at:",pkl_file)
+        with gzip.open(pkl_file) as fin:
+            hin = pickle.load(fin)
+            print(hin.values())
+
+def main():
+
+    # Dump the values currently in the committed fliprates pkl files
+    dump_vals(pkl_lst_committed)
+
+    # Dump the values currently in this dir
+    #dump_vals(pkl_lst_local)
+
+    '''
+    # Example of looking up values using the dense lookup tools
+    with gzip.open("test_ratio.pkl.gz") as fin:
         hin = pickle.load(fin)
         print(hin.values())
+        print(hin.axis("pt").edges())
+        print(hin.axis("eta").edges())
+        x = lookup_tools.dense_lookup.dense_lookup(hin.values()[()],[hin.axis("pt").edges(),hin.axis("eta").edges()])
+        print("x",x)
+        print("x",type(x))
+        print(x(-35,1.9))
+        print(x(-35,-7))
+        print(x(500,1.9))
+    '''
 
-'''
-# Example of looking up values using the dense lookup tools
-with gzip.open("test_ratio.pkl.gz") as fin:
-    hin = pickle.load(fin)
-    print(hin.values())
-    print(hin.axis("pt").edges())
-    print(hin.axis("eta").edges())
-    x = lookup_tools.dense_lookup.dense_lookup(hin.values()[()],[hin.axis("pt").edges(),hin.axis("eta").edges()])
-    print("x",x)
-    print("x",type(x))
-    print(x(-35,1.9))
-    print(x(-35,-7))
-    print(x(500,1.9))
-'''
+main()
