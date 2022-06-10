6dbf8b38e596e539257e9e16fc2e47a7313d8325
Andrew Hennessee <ahennes3@cclws18.cse.nd.edu>
2022-06-10 13:43:56 -0400

    Minor changes in reading log

--- topcoffea/scripts/topEFT_workflow_viz.py
+++ topcoffea/scripts/topEFT_workflow_viz.py
@@ -14,63 +14,77 @@ from hurry.filesize import size
 
 # Functions
 
-def max(log_file):
-    max_cpu, max_mem, max_out = [0,0,0]
+def parse(log_file):
+    tasks = []
+    max_cpu = 0
+    max_mem = 0
+    max_out = 0
 
+    next(log_file) # Skip header
     for task in log_file:
         task = task.strip().split(',')
-        _, category, status, _, _, _, _, _, _, _, cpu_time, memory, _, fout = task
+        task_info = { # See tasks_accum_log
+            'task_id'      : task[0],
+            'category'     : task[1],
+            'status'       : task[2],
+            'range_start'  : float(task[5]),
+            'range_stop'   : float(task[6]),
+            'accum_parent' : task[7],
+            'time_start'   : float(task[8]),
+            'time_end'     : float(task[9]),
+            'cpu_time'     : float(task[10]),
+            'memory'       : float(task[11]),
+            'fout'         : float(task[13]),
+        }
 
-        if (status == 'accumulated') and (not category == 'preprocessing'):
-            max_cpu  = float(cpu_time) if float(cpu_time) > max_cpu else max_cpu
-            max_mem  = float(memory) if float(memory) > max_mem else max_mem
-            max_out  = float(fout) if float(fout) > max_out else max_out
+        # Track accumulated tasks, ignore preprocessing
+        # Determine max CPU time, memory usage, and output size (for scaling)
+        if (task_info['status'] == 'accumulated') and (not task_info['category'] == 'preprocessing'):
+            tasks.append(task_info)
+            max_cpu = task_info['cpu_time'] if task_info['cpu_time'] > max_cpu else max_cpu
+            max_mem = task_info['memory'] if task_info['memory'] > max_mem else max_mem
+            max_out = task_info['fout'] if task_info['fout'] > max_out else max_out
 
-    return max_cpu, max_mem, max_out
+    return tasks, max_cpu, max_mem, max_out
 
-def parse_log_file(log_file, workflow, max_cpu, max_mem, max_out):
-    for task in log_file:
-        task = task.strip().split(',')
-        task_id, category, status, _, _, range_start, range_stop, accum_parent, time_start, time_end, cpu_time, memory, _, fout = task
+def make_graph(tasks, workflow, max_cpu, max_mem, max_out):
+    # Create nodes and directed edges between processing and accumulation tasks
+    for task in tasks:
+        # Scaling variables
+        func_size  = math.sqrt(task['memory']/max_mem)
+        func_color = task['cpu_time']/max_cpu
+        fout_size  = task['fout']/max_out
 
-        # Create nodes and directed edges between processing and accumulation tasks
-        # Tasks must have a status of "accumulated"
-        # Ignore preprocessing tasks
-        if (status == 'accumulated') and (not category == 'preprocessing'):
-            func_size  = math.sqrt(float(memory)/max_mem)
-            func_color = float(cpu_time)/max_cpu
-            fout_size  = float(fout)/max_out
+        # Function node
+        func_node_info = f'''ID: {task['task_id']}{task['category'][0]}
+CPU: {round(task['cpu_time'], 3)}s
+Wall: {round(task['time_end'] - task['time_start'], 3)}s
+{task['memory']} MB
+{task['range_stop'] - task['range_start']}'''
 
-            # Function node
-            func_node_info = f'''ID: {task_id}{category[0]}
-CPU: {round(float(cpu_time), 3)}s
-Wall: {round(float(time_end) - float(time_start), 3)}s
-{memory} MB
-{float(range_stop) - float(range_start)}'''
+        workflow.node(task['task_id'], label=func_node_info,
+            width       = f'{(func_size + 1)*30}',
+            height      = f'{(func_size + 1)*30}',
+            fontsize    = '300',
+            fontname    = 'Helvetica',
+            colorscheme = 'rdylgn11',
+            style       = 'filled',
+            color       = f'{11 - int(func_color*10)}')
 
-            workflow.node(task_id, label=func_node_info,
-                    width       = f'{(func_size + 1)*30}',
-                    height      = f'{(func_size + 1)*30}',
-                    fontsize    = '300',
-                    fontname    = 'Helvetica',
-                    colorscheme = 'rdylgn11',
-                    style       = 'filled',
-                    color       = f'{11 - int(func_color*10)}')
+        # Output node
+        workflow.node(f'''{task['task_id']}{task['fout']}''', label=f'''{size(task['fout'])}B''',
+            shape       = 'box',
+            width       = f'{(fout_size + 1)*30}',
+            height      = f'{(fout_size + 1)*30}',
+            fontsize    = '500',
+            fontname    = 'Helvetica')
 
-            # Output node
-            workflow.node(f'{task_id}{fout}', label=f'{size(float(fout))}B',
-                    shape       = 'box',
-                    width       = f'{(fout_size + 1)*30}',
-                    height      = f'{(fout_size + 1)*30}',
-                    fontsize    = '500',
-                    fontname    = 'Helvetica')
+        # Edge between function and output
+        workflow.edge(task['task_id'], f'''{task['task_id']}{task['fout']}''')
 
-            # Edge between function and output
-            workflow.edge(task_id, f'{task_id}{fout}')
-
-            # Edge between output and accumulation
-            if not accum_parent == '0':
-                workflow.edge(f'{task_id}{fout}', accum_parent)
+        # Edge between output and accumulation
+        if not task['accum_parent'] == '0':
+            workflow.edge(f'''{task['task_id']}{task['fout']}''', task['accum_parent'])
 
     return workflow
 
@@ -85,23 +99,19 @@ def generate_viz(log_file):
 # Main execution
 
 def main():
-    # Open log file
+    # Parse log file to collect info for processing and accumulating tasks
     log_file = open(sys.argv[1], 'r')
+    tasks, max_cpu, max_mem, max_out = parse(log_file)
+    log_file.close()
 
-    # Determine max CPU time, memory, and output size usage for coloring/scaling
-    max_cpu, max_mem, max_out = max(log_file)
-    log_file.seek(0) # Reset file handle to beginning of file
-
-    # Parse log file and generate DOT object
+    # Generate DOT object
     workflow = gv.Digraph('TopEFT Workflow', graph_attr={'rankdir': 'LR', 'size': '85,110', 'ratio': 'fill'})
-    workflow = parse_log_file(log_file, workflow, max_cpu, max_mem, max_out)
+    workflow = make_graph(tasks, workflow, max_cpu, max_mem, max_out)
 
     # Generate DOT (.gv) and visualization (.pdf) files
     # .gv files contain DOT syntax to organize the graph
     generate_dot(workflow)
     generate_viz(sys.argv[1])
-
-    log_file.close()
 
 if __name__ == '__main__':
     main()
