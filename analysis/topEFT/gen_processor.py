8f9c85f7bc7a24929442c73cbdc8c303decbe821
Kelci Mohrman <kmohrman@nd.edu>
2022-06-12 17:55:56 -0400

    Adding the rest of the histos used in the mc validation studies

--- analysis/topEFT/gen_processor.py
+++ analysis/topEFT/gen_processor.py
@@ -16,7 +16,7 @@ from coffea.lumi_tools import LumiMask
 
 from topcoffea.modules.GetValuesFromJsons import get_lumi
 from topcoffea.modules.objects import *
-from topcoffea.modules.corrections import SFevaluator, GetBTagSF, ApplyJetCorrections, GetBtagEff, AttachMuonSF, AttachElectronSF, AttachPerLeptonFR, GetPUSF, ApplyRochesterCorrections, ApplyJetSystematics, AttachPSWeights, AttachPdfWeights, AttachScaleWeights, GetTriggerSF
+from topcoffea.modules.corrections import SFevaluator, GetBTagSF, ApplyJetCorrections, GetBtagEff, AttachMuonSF, AttachElectronSF, AttachPerLeptonFR, GetPUSF, ApplyRochesterCorrections, ApplyJetSystematics, AttachPSWeights, AttachPdfWeights, AttachScaleWeights, GetTriggerSF, get_ht_sf
 from topcoffea.modules.selection import *
 from topcoffea.modules.HistEFT import HistEFT
 from topcoffea.modules.paths import topcoffea_path
@@ -36,8 +36,12 @@ class AnalysisProcessor(processor.ProcessorABC):
             "mll_fromzg_e" : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("mll_fromzg_e", "invmass ee from z/gamma", 40, 0, 200)),
             "mll_fromzg_m" : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("mll_fromzg_m", "invmass mm from z/gamma", 40, 0, 200)),
             "mll_fromzg_t" : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("mll_fromzg_t", "invmass tautau from z/gamma", 40, 0, 200)),
-            "mll" : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("mll", "invmass l0l1", 60, 0, 600)),
-            "njets" : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("njets", "njets", 10, 0, 10)),
+            "mll"          : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("mll", "Invmass l0l1", 60, 0, 600)),
+            "ht"           : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("ht", "Scalar sum of genjet pt", 100, 0, 1000)),
+            "ht_clean"     : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("ht_clean", "Scalar sum of clean genjet pt", 100, 0, 1000)),
+            "tops_pt"      : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("tops_pt", "Pt of the sum of the tops", 50, 0, 500)),
+            "tX_pt"        : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("tX_pt", "Pt of the t(t)X system", 40, 0, 400)),
+            "njets"        : HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("njets", "njets", 10, 0, 10)),
         })
 
         # Set the list of hists to fill
@@ -51,15 +55,8 @@ class AnalysisProcessor(processor.ProcessorABC):
                     raise Exception(f"Error: Cannot specify hist \"{hist_to_include}\", it is not defined in the processor.")
             self._hist_lst = hist_lst # Which hists to fill
 
-        # Set the energy threshold to cut on
-        self._ecut_threshold = ecut_threshold
-
         # Set the booleans
         self._do_errors = do_errors # Whether to calculate and store the w**2 coefficients
-        self._do_systematics = do_systematics # Whether to process systematic samples
-        self._split_by_lepton_flavor = split_by_lepton_flavor # Whether to keep track of lepton flavors individually
-        self._skip_signal_regions = skip_signal_regions # Whether to skip the SR categories
-        self._skip_control_regions = skip_control_regions # Whether to skip the CR categories
 
 
     @property
@@ -93,29 +90,42 @@ class AnalysisProcessor(processor.ProcessorABC):
 
         is_final_mask = genpart.hasFlags(["fromHardProcess","isLastCopy"])
 
+        gen_top = ak.pad_none(genpart[is_final_mask & (abs(genpart.pdgId) == 6)],2)
+
         gen_l = genpart[is_final_mask & ((abs(genpart.pdgId) == 11) | (abs(genpart.pdgId) == 13) | (abs(genpart.pdgId) == 15))]
         gen_e = genpart[is_final_mask & (abs(genpart.pdgId) == 11)]
         gen_m = genpart[is_final_mask & (abs(genpart.pdgId) == 13)]
         gen_t = genpart[is_final_mask & (abs(genpart.pdgId) == 15)]
 
         gen_l = gen_l[ak.argsort(gen_l.pt, axis=-1, ascending=False)]
+        gen_l = ak.pad_none(gen_l,2)
         gen_e = gen_e[ak.argsort(gen_e.pt, axis=-1, ascending=False)]
         gen_m = gen_m[ak.argsort(gen_m.pt, axis=-1, ascending=False)]
         gen_t = gen_t[ak.argsort(gen_t.pt, axis=-1, ascending=False)]
 
-        gen_l = ak.pad_none(gen_l,2)
-
+        gen_l_from_zg = ak.pad_none(gen_l[(gen_l.distinctParent.pdgId == 23) | (gen_l.distinctParent.pdgId == 22)], 2)
         gen_e_from_zg = ak.pad_none(gen_e[(gen_e.distinctParent.pdgId == 23) | (gen_e.distinctParent.pdgId == 22)], 2)
         gen_m_from_zg = ak.pad_none(gen_m[(gen_m.distinctParent.pdgId == 23) | (gen_m.distinctParent.pdgId == 22)], 2)
         gen_t_from_zg = ak.pad_none(gen_t[(gen_t.distinctParent.pdgId == 23) | (gen_t.distinctParent.pdgId == 22)], 2)
 
+
         # Jet object selection
-        is_clean_jet = isClean(genjet, gen_e, drmin=0.4) & isClean(genjet, gen_m, drmin=0.4) & isClean(genjet, gen_t, drmin=0.4) & (genjet.pt > 30.0)
+        genjet = genjet[genjet.pt > 30]
+        is_clean_jet = isClean(genjet, gen_e, drmin=0.4) & isClean(genjet, gen_m, drmin=0.4) & isClean(genjet, gen_t, drmin=0.4)
         genjet_clean = genjet[is_clean_jet]
         njets = ak.num(genjet_clean)
 
 
         ### Get dense axis variables ###
+
+        ht = ak.sum(genjet.pt,axis=-1)
+        ht_clean = ak.sum(genjet_clean.pt,axis=-1)
+
+        tops_pt = gen_top.sum().pt
+
+        # Pt of the t(t)X system
+        tX_system = ak.concatenate([gen_top,gen_l_from_zg],axis=1)
+        tX_pt = tX_system.sum().pt
 
         # Invmass distributions
         mll_e_from_zg = (gen_e_from_zg[:,0] + gen_e_from_zg[:,1]).mass
@@ -129,6 +139,10 @@ class AnalysisProcessor(processor.ProcessorABC):
             "mll_fromzg_m" : mll_m_from_zg,
             "mll_fromzg_t" : mll_t_from_zg,
             "mll" : mll_l0l1,
+            "ht" : ht,
+            "ht_clean" : ht_clean,
+            "tX_pt" : tX_pt,
+            "tops_pt" : tops_pt,
             "njets" : njets,
         }
 
@@ -150,6 +164,11 @@ class AnalysisProcessor(processor.ProcessorABC):
         lumi = get_lumi(year)*1000.0
         event_weight = lumi*xsec*genw/sow
 
+        # Example of reweighting based on Ht 
+        #if "private" in histAxisName:
+        #    ht_sf = get_ht_sf(ht,histAxisName)
+        #    event_weight = event_weight*ht_sf
+
 
         ### Loop over the hists we want to fill ###
 
@@ -158,7 +177,6 @@ class AnalysisProcessor(processor.ProcessorABC):
         for dense_axis_name, dense_axis_vals in dense_axis_dict.items():
 
             # Mask out the none values
-            #isnotnone_mask = (ak.fill_none((dense_axis_vals != None),False) & nlep>=2)
             isnotnone_mask = (ak.fill_none((dense_axis_vals != None),False))
             dense_axis_vals_cut = dense_axis_vals[isnotnone_mask]
             event_weight_cut = event_weight[isnotnone_mask]
