1258991df20518e925544d441e3824ef5d80ccba
Brent Yates <yates.313@osu.edu>
2022-06-21 11:32:25 -0400

    Removed 'JES_' (not used in this version)

--- analysis/topEFT/topeft.py
+++ analysis/topEFT/topeft.py
@@ -276,7 +276,7 @@ class AnalysisProcessor(processor.ProcessorABC):
         # Define the lists of systematics we include
         obj_correction_syst_lst = [
             f'JER_{year}Up',f'JER_{year}Down', # Systs that affect the kinematics of objects
-            'JES_FlavorPureGluon', 'JES_FlavorPureQuark', 'JES_FlavorPureCharm', 'JES_FlavorPureBottom', 'JES_TotalNoFlavor'
+            'FlavorPureGluon', 'FlavorPureQuark', 'FlavorPureCharm', 'FlavorPureBottom', 'TotalNoFlavor'
         ]
         wgt_correction_syst_lst = [
             "lepSF_muonUp","lepSF_muonDown","lepSF_elecUp","lepSF_elecDown",f"btagSFbc_{year}Up",f"btagSFbc_{year}Down","btagSFbc_corrUp","btagSFbc_corrDown",f"btagSFlight_{year}Up",f"btagSFlight_{year}Down","btagSFlight_corrUp","btagSFlight_corrDown","PUUp","PUDown","PreFiringUp","PreFiringDown",f"triggerSF_{year}Up",f"triggerSF_{year}Down", # Exp systs
