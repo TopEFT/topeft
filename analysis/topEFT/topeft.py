a4497dc7ea1f2ce1e0576f151b75611a080ebee0
Brent Yates <yates.313@osu.edu>
2022-06-21 11:12:34 -0400

    Switching to POG recommended JEC

--- analysis/topEFT/topeft.py
+++ analysis/topEFT/topeft.py
@@ -276,7 +276,7 @@ class AnalysisProcessor(processor.ProcessorABC):
         # Define the lists of systematics we include
         obj_correction_syst_lst = [
             f'JER_{year}Up',f'JER_{year}Down', # Systs that affect the kinematics of objects
-            'JES_FlavorQCDUp', 'JES_RelativeBalUp', 'JES_HFUp', 'JES_BBEC1Up', 'JES_EC2Up', 'JES_AbsoluteUp', f'JES_BBEC1_{year}Up', f'JES_EC2_{year}Up', f'JES_Absolute_{year}Up', f'JES_HF_{year}Up', f'JES_RelativeSample_{year}Up','JES_FlavorQCDDown', 'JES_RelativeBalDown', 'JES_HFDown', 'JES_BBEC1Down', 'JES_EC2Down', 'JES_AbsoluteDown', f'JES_BBEC1_{year}Down', f'JES_EC2_{year}Down', f'JES_Absolute_{year}Down', f'JES_HF_{year}Down', f'JES_RelativeSample_{year}Down' # Systs that affect the kinematics of objects
+            'JES_FlavorPureGluon', 'JES_FlavorPureQuark', 'JES_FlavorPureCharm', 'JES_FlavorPureBottom', 'JES_SubTotalPileUp', 'JES_SubTotalRelative', 'JES_SubTotalPt', 'JES_SubTotalScale', 'JES_SubTotalAbsolute', 'JES_SubTotalMC'
         ]
         wgt_correction_syst_lst = [
             "lepSF_muonUp","lepSF_muonDown","lepSF_elecUp","lepSF_elecDown",f"btagSFbc_{year}Up",f"btagSFbc_{year}Down","btagSFbc_corrUp","btagSFbc_corrDown",f"btagSFlight_{year}Up",f"btagSFlight_{year}Down","btagSFlight_corrUp","btagSFlight_corrDown","PUUp","PUDown","PreFiringUp","PreFiringDown",f"triggerSF_{year}Up",f"triggerSF_{year}Down", # Exp systs
