e266cbefbf12629161baa32f14f4f06ef2ef8dec
Kelci Mohrman <kmohrman@nd.edu>
2022-08-03 08:32:32 -0400

    Initial commit of a processor to look at data

---
+++ analysis/data_comparisons/data_comp.py
@@ -0,0 +1,308 @@
+#!/usr/bin/env python
+import lz4.frame as lz4f
+import cloudpickle
+import json
+import pprint
+import copy
+import coffea
+import numpy as np
+import awkward as ak
+np.seterr(divide='ignore', invalid='ignore', over='ignore')
+from coffea import hist, processor
+from coffea.util import load, save
+from optparse import OptionParser
+from coffea.analysis_tools import PackedSelection
+from coffea.lumi_tools import LumiMask
+
+from topcoffea.modules.GetValuesFromJsons import get_param
+from topcoffea.modules.objects import *
+from topcoffea.modules.corrections import SFevaluator, GetBTagSF, ApplyJetCorrections, GetBtagEff, AttachMuonSF, AttachElectronSF, AttachPerLeptonFR, GetPUSF, ApplyRochesterCorrections, ApplyJetSystematics, AttachPSWeights, AttachPdfWeights, AttachScaleWeights, GetTriggerSF
+from topcoffea.modules.selection import *
+from topcoffea.modules.HistEFT import HistEFT
+from topcoffea.modules.paths import topcoffea_path
+import topcoffea.modules.eft_helper as efth
+
+
+class AnalysisProcessor(processor.ProcessorABC):
+
+    def __init__(self, samples, wc_names_lst=[], hist_lst=None, ecut_threshold=None, do_errors=False, do_systematics=False, split_by_lepton_flavor=False, skip_signal_regions=False, skip_control_regions=False, muonSyst='nominal', dtype=np.float32):
+
+        self._samples = samples
+        self._wc_names_lst = wc_names_lst
+        self._dtype = dtype
+
+        # Create the histograms
+        self._accumulator = processor.dict_accumulator({
+            "ljptsum" : hist.Hist("Events", hist.Cat("sample", "sample"), hist.Cat("channel", "channel"), hist.Bin("ljptsum", "S$_{T}$ (GeV)", 11, 0, 1100)),
+        })
+
+    @property
+    def accumulator(self):
+        return self._accumulator
+
+    @property
+    def columns(self):
+        return self._columns
+
+    # Main function: run on a given dataset
+    def process(self, events):
+
+        # Dataset parameters
+        dataset = events.metadata["dataset"]
+
+        isData             = self._samples[dataset]["isData"]
+        histAxisName       = self._samples[dataset]["histAxisName"]
+        year               = self._samples[dataset]["year"]
+
+        datasets = ["SingleMuon", "SingleElectron", "EGamma", "MuonEG", "DoubleMuon", "DoubleElectron", "DoubleEG"]
+        for d in datasets: 
+            if d in dataset: dataset = dataset.split('_')[0]
+
+        # Set the sampleType (used for MC matching requirement)
+        sampleType = "prompt"
+        if isData:
+            sampleType = "data"
+        elif histAxisName in get_param("conv_samples"):
+            sampleType = "conversions"
+        elif histAxisName in get_param("prompt_and_conv_samples"):
+            # Just DY (since we care about prompt DY for Z CR, and conv DY for 3l CR)
+            sampleType = "prompt_and_conversions"
+
+        # Initialize objects
+        met  = events.MET
+        e    = events.Electron
+        mu   = events.Muon
+        tau  = events.Tau
+        jets = events.Jet
+
+        e["idEmu"] = ttH_idEmu_cuts_E3(e.hoe, e.eta, e.deltaEtaSC, e.eInvMinusPInv, e.sieie)
+        e["conept"] = coneptElec(e.pt, e.mvaTTHUL, e.jetRelIso)
+        mu["conept"] = coneptMuon(mu.pt, mu.mvaTTHUL, mu.jetRelIso, mu.mediumId)
+        e["btagDeepFlavB"] = ak.fill_none(e.matched_jet.btagDeepFlavB, -99)
+        mu["btagDeepFlavB"] = ak.fill_none(mu.matched_jet.btagDeepFlavB, -99)
+
+        if not isData:
+            e["gen_pdgId"] = ak.fill_none(e.matched_gen.pdgId, 0)
+            mu["gen_pdgId"] = ak.fill_none(mu.matched_gen.pdgId, 0)
+
+        ################### Electron selection ####################
+
+        e["isPres"] = isPresElec(e.pt, e.eta, e.dxy, e.dz, e.miniPFRelIso_all, e.sip3d, getattr(e,"mvaFall17V2noIso_WPL"))
+        e["isLooseE"] = isLooseElec(e.miniPFRelIso_all,e.sip3d,e.lostHits)
+        e["isFO"] = isFOElec(e.pt, e.conept, e.btagDeepFlavB, e.idEmu, e.convVeto, e.lostHits, e.mvaTTHUL, e.jetRelIso, e.mvaFall17V2noIso_WP90, year)
+        e["isTightLep"] = tightSelElec(e.isFO, e.mvaTTHUL)      
+
+        ################### Muon selection ####################
+
+        mu["pt"] = ApplyRochesterCorrections(year, mu, isData) # Need to apply corrections before doing muon selection
+        mu["isPres"] = isPresMuon(mu.dxy, mu.dz, mu.sip3d, mu.eta, mu.pt, mu.miniPFRelIso_all)
+        mu["isLooseM"] = isLooseMuon(mu.miniPFRelIso_all,mu.sip3d,mu.looseId)
+        mu["isFO"] = isFOMuon(mu.pt, mu.conept, mu.btagDeepFlavB, mu.mvaTTHUL, mu.jetRelIso, year)
+        mu["isTightLep"]= tightSelMuon(mu.isFO, mu.mediumId, mu.mvaTTHUL)
+
+        ################### Loose selection ####################
+
+        m_loose = mu[mu.isPres & mu.isLooseM]
+        e_loose = e[e.isPres & e.isLooseE]
+        l_loose = ak.with_name(ak.concatenate([e_loose, m_loose], axis=1), 'PtEtaPhiMCandidate')
+
+        # Compute pair invariant masses, for all flavors all signes
+        llpairs = ak.combinations(l_loose, 2, fields=["l0","l1"])
+        events["minMllAFAS"] = ak.min( (llpairs.l0+llpairs.l1).mass, axis=-1)
+
+        # Build FO collection
+        m_fo = mu[mu.isPres & mu.isLooseM & mu.isFO]
+        e_fo = e[e.isPres & e.isLooseE & e.isFO]
+
+        # Attach the lepton SFs to the electron and muons collections
+        AttachElectronSF(e_fo,year=year)
+        AttachMuonSF(m_fo,year=year)
+
+        # Attach per lepton fake rates
+        AttachPerLeptonFR(e_fo, flavor = "Elec", year=year)
+        AttachPerLeptonFR(m_fo, flavor = "Muon", year=year)
+        m_fo['convVeto'] = ak.ones_like(m_fo.charge); 
+        m_fo['lostHits'] = ak.zeros_like(m_fo.charge); 
+        l_fo = ak.with_name(ak.concatenate([e_fo, m_fo], axis=1), 'PtEtaPhiMCandidate')
+        l_fo_conept_sorted = l_fo[ak.argsort(l_fo.conept, axis=-1,ascending=False)]
+
+        #################### Jets ####################
+
+        # Jet cleaning, before any jet selection
+        vetos_tocleanjets = ak.with_name( l_fo, "PtEtaPhiMCandidate")
+        tmp = ak.cartesian([ak.local_index(jets.pt), vetos_tocleanjets.jetIdx], nested=True)
+        cleanedJets = jets[~ak.any(tmp.slot0 == tmp.slot1, axis=-1)] # this line should go before *any selection*, otherwise lep.jetIdx is not aligned with the jet index
+
+        # Selecting jets and cleaning them
+        jetptname = "pt_nom" if hasattr(cleanedJets, "pt_nom") else "pt"
+        cleanedJets["isGood"] = isTightJet(getattr(cleanedJets, jetptname), cleanedJets.eta, cleanedJets.jetId, jetPtCut=30.) # temporary at 25 for synch, TODO: Do we want 30 or 25?
+        goodJets = cleanedJets[cleanedJets.isGood]
+
+        # Count jets
+        njets = ak.num(goodJets)
+        ht = ak.sum(goodJets.pt,axis=-1)
+        j0 = goodJets[ak.argmax(goodJets.pt,axis=-1,keepdims=True)]
+
+        # Loose DeepJet WP
+        if year == "2017":
+            btagwpl = get_param("btag_wp_loose_UL17")
+            btagwpm = get_param("btag_wp_medium_UL17")
+        elif year == "2018":
+            btagwpl = get_param("btag_wp_loose_UL18")
+            btagwpm = get_param("btag_wp_medium_UL18")
+        elif year=="2016":
+            btagwpl = get_param("btag_wp_loose_UL16")          
+            btagwpm = get_param("btag_wp_medium_UL16")
+        elif year=="2016APV":
+            btagwpl = get_param("btag_wp_loose_UL16APV")
+            btagwpm = get_param("btag_wp_medium_UL16APV")
+        else:
+            raise ValueError(f"Error: Unknown year \"{year}\".")
+
+        isBtagJetsLoose = (goodJets.btagDeepFlavB > btagwpl)
+        nbtagsl = ak.num(goodJets[isBtagJetsLoose])
+
+        isBtagJetsMedium = (goodJets.btagDeepFlavB > btagwpm)
+        nbtagsm = ak.num(goodJets[isBtagJetsMedium])
+
+
+        #################### Add variables into event object so that they persist ####################
+
+        # Put njets and l_fo_conept_sorted into events
+        events["njets"] = njets
+        events["l_fo_conept_sorted"] = l_fo_conept_sorted
+
+        # The event selection
+        add2lMaskAndSFs(events, year, isData, sampleType)
+        add3lMaskAndSFs(events, year, isData, sampleType)
+        add4lMaskAndSFs(events, year, isData)
+        addLepCatMasks(events)
+
+        # Convenient to have l0, l1, l2 on hand
+        l_fo_conept_sorted_padded = ak.pad_none(l_fo_conept_sorted, 3)
+        l0 = l_fo_conept_sorted_padded[:,0]
+        l1 = l_fo_conept_sorted_padded[:,1]
+        l2 = l_fo_conept_sorted_padded[:,2]
+
+        ######### Masks we need for the selection ##########
+
+        # Get the lumi mask for data
+        if year == "2016" or year == "2016APV":
+            golden_json_path = topcoffea_path("data/goldenJsons/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt")
+        elif year == "2017":
+            golden_json_path = topcoffea_path("data/goldenJsons/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt")
+        elif year == "2018":
+            golden_json_path = topcoffea_path("data/goldenJsons/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt")
+        else:
+            raise ValueError(f"Error: Unknown year \"{year}\".")
+        lumi_mask = LumiMask(golden_json_path)(events.run,events.luminosityBlock)
+
+        # Get mask for events that have two sf os leps close to z peak
+        sfosz_3l_mask = get_Z_peak_mask(l_fo_conept_sorted_padded[:,0:3],pt_window=10.0)
+
+        # Pass trigger mask
+        pass_trg = trgPassNoOverlap(events,isData,dataset,str(year))
+
+        # b jet masks
+        bmask_atleast1med_atleast2loose = ((nbtagsm>=1)&(nbtagsl>=2)) # Used for 2lss and 4l
+        bmask_exactly1med = (nbtagsm==1) # Used for 3l SR and 2lss CR
+        bmask_atleast2med = (nbtagsm>=2) # Used for 3l SR
+        bmask_atmost2med  = (nbtagsm< 3) # Used to make 2lss mutually exclusive from tttt enriched
+        bmask_atleast3med = (nbtagsm>=3) # Used for tttt enriched
+
+        # Charge masks
+        chargel0_p = ak.fill_none(((l0.charge)>0),False)
+        chargel0_m = ak.fill_none(((l0.charge)<0),False)
+        charge2l_1 = ak.fill_none(((l0.charge+l1.charge)!=0),False)
+        charge3l_p = ak.fill_none(((l0.charge+l1.charge+l2.charge)>0),False)
+        charge3l_m = ak.fill_none(((l0.charge+l1.charge+l2.charge)<0),False)
+
+        # Store the masks for each of the 11 signal region categories using the PackedSelection object
+        selections = PackedSelection(dtype='uint64')
+        # 2lss selection
+        selections.add("2lss_p_2b", (events.is2l & events.is2l_SR & chargel0_p & charge2l_1 & bmask_atleast1med_atleast2loose & bmask_atmost2med  & (njets>=4) & pass_trg))
+        selections.add("2lss_m_2b", (events.is2l & events.is2l_SR & chargel0_m & charge2l_1 & bmask_atleast1med_atleast2loose & bmask_atmost2med  & (njets>=4) & pass_trg))
+        selections.add("2lss_p_3b", (events.is2l & events.is2l_SR & chargel0_p & charge2l_1 & bmask_atleast1med_atleast2loose & bmask_atleast3med & (njets>=4) & pass_trg))
+        selections.add("2lss_m_3b", (events.is2l & events.is2l_SR & chargel0_m & charge2l_1 & bmask_atleast1med_atleast2loose & bmask_atleast3med & (njets>=4) & pass_trg))
+        # 3l selection
+        selections.add("3l_p_offZ_1b", (events.is3l & events.is3l_SR & charge3l_p & ~sfosz_3l_mask & bmask_exactly1med & (njets>=2) & pass_trg))
+        selections.add("3l_m_offZ_1b", (events.is3l & events.is3l_SR & charge3l_m & ~sfosz_3l_mask & bmask_exactly1med & (njets>=2) & pass_trg))
+        selections.add("3l_p_offZ_2b", (events.is3l & events.is3l_SR & charge3l_p & ~sfosz_3l_mask & bmask_atleast2med & (njets>=2) & pass_trg))
+        selections.add("3l_m_offZ_2b", (events.is3l & events.is3l_SR & charge3l_m & ~sfosz_3l_mask & bmask_atleast2med & (njets>=2) & pass_trg))
+        selections.add("3l_onZ_1b",    (events.is3l & events.is3l_SR &               sfosz_3l_mask & bmask_exactly1med & (njets>=2) & pass_trg))
+        selections.add("3l_onZ_2b",    (events.is3l & events.is3l_SR &               sfosz_3l_mask & bmask_atleast2med & (njets>=2) & pass_trg))
+        # 4l selection
+        selections.add("4l", (events.is4l & events.is4l_SR & bmask_atleast1med_atleast2loose & (njets>=2) & pass_trg))
+
+
+        sr_category_lst = [
+            "2lss_m_2b",
+            "2lss_p_2b",
+            "2lss_m_3b",
+            "2lss_p_3b",
+            "3l_onZ_1b",
+            "3l_onZ_2b",
+            "3l_p_offZ_1b",
+            "3l_m_offZ_1b",
+            "3l_p_offZ_2b",
+            "3l_m_offZ_2b",
+            "4l",
+        ]
+
+        # Construct a mask for events that passes the selection for any of our signal regions
+        sr_event_mask = selections.any(*sr_category_lst)
+        if isData: sr_event_mask = (sr_event_mask & lumi_mask)
+
+        ######### Get info about the events ##########
+
+        tight_lep_mask = ak.fill_none(l_fo_conept_sorted_padded.isTightLep,False)
+        tight_lep = l_fo_conept_sorted_padded[tight_lep_mask]
+        tight_lep = tight_lep[sr_event_mask]
+
+        run = events.run
+        luminosityBlock = events.luminosityBlock
+        event = events.event
+
+        print("run",run)
+        print("luminosityBlock",luminosityBlock)
+        print("event",event)
+        print("")
+
+        ######### PLACEHOLDER Filling the histo ##########
+
+        # We probably don't want to use a histo for this processor
+        # But I'll leave the histo filling stuff here for now as a placeholder
+
+        # Collection of all objects (leptons and jets)
+        l_j_collection = ak.with_name(ak.concatenate([l_fo_conept_sorted,goodJets], axis=1),"PtEtaPhiMCollection")
+        ljptsum = ak.sum(l_j_collection.pt,axis=-1)
+
+        # Variables we will loop over when filling hists
+        varnames = {}
+        varnames["ljptsum"] = ljptsum
+
+        # Initialize the out object
+        hout = self.accumulator.identity()
+
+        # Loop over the hists we want to fill
+        for dense_axis_name, dense_axis_vals in varnames.items():
+
+            # Loop over the channels in each nlep cat (e.g. "3l_m_offZ_1b")
+            for sr_category in sr_category_lst:
+
+                    cuts_lst = [sr_category]
+                    all_cuts_mask = selections.all(*cuts_lst)
+
+                    # Fill the histos
+                    axes_fill_info_dict = {
+                        dense_axis_name : dense_axis_vals[all_cuts_mask],
+                        "channel"       : sr_category,
+                        "sample"        : histAxisName,
+                    }
+
+                    hout[dense_axis_name].fill(**axes_fill_info_dict)
+
+        return hout
+
+    def postprocess(self, accumulator):
+        return accumulator
---
+++ analysis/data_comparisons/run_data_comp.py
@@ -0,0 +1,165 @@
+import json
+import time
+import cloudpickle
+import gzip
+import os
+import argparse
+
+import numpy as np
+from coffea import processor
+from coffea.nanoevents import NanoAODSchema
+import topcoffea.modules.remote_environment as remote_environment
+
+import data_comp
+
+from topcoffea.modules.utils import load_sample_json_file, read_cfg_file, update_cfg
+
+parser = argparse.ArgumentParser(description='You can customize your run')
+parser.add_argument('inputFiles'            , nargs='?', default='', help = 'Json or cfg file(s) containing files and metadata')
+parser.add_argument('--executor','-x'       , default='work_queue', help = 'Which executor to use')
+parser.add_argument('--chunksize','-s'      , default=100000, type=int, help = 'Number of events per chunk')
+parser.add_argument('--max-files','-N'      , default=0, type=int, help = 'If specified, limit the number of root files per sample. Useful for testing')
+parser.add_argument('--nchunks','-c'        , default=0, type=int, help = 'You can choose to run only a number of chunks')
+parser.add_argument('--outname','-o'        , default='flipTopEFT', help = 'Name of the output file with histograms')
+parser.add_argument('--outpath','-p'        , default='histos', help = 'Name of the output directory')
+parser.add_argument('--treename'            , default='Events', help = 'Name of the tree inside the files')
+parser.add_argument('--xrd'                 , default='', help = 'The XRootD redirector to use when reading directly from json files')
+
+args = parser.parse_args()
+inputFiles = args.inputFiles.replace(' ','').split(',')  # Remove whitespace and split by commas
+executor   = args.executor
+chunksize  = args.chunksize
+nchunks    = args.nchunks if args.nchunks else None
+outname    = args.outname
+outpath    = args.outpath
+treename   = args.treename
+xrd        = args.xrd
+max_files  = args.max_files
+
+
+samples_to_process = {}
+for fn in inputFiles:
+    if fn.endswith('.json'):
+        sample = os.path.basename(fn).replace('.json','')
+        jsn = load_sample_json_file(fn)
+        samples_to_process = update_cfg(jsn,
+            name=sample,
+            cfg=samples_to_process,
+            max_files=max_files,
+            redirector=xrd
+        )
+    elif fn.endswith('.cfg'):
+        samples_to_process = read_cfg_file(fn,cfg=samples_to_process,max_files=max_files)
+    else:
+        raise RuntimeError(f"Unknown input file: {fn}")
+
+flist = {}
+for sample_name,jsn in samples_to_process.items():
+    #if jsn['WCnames'] != []: raise Exception(f"Error: This processor is not set up to handle EFT samples.")
+    xrd_src = jsn['redirector']
+    flist[sample_name] = [f"{xrd_src}{fn}" for fn in jsn['files']]
+
+    jsn_txt = json.dumps(jsn,indent=2,sort_keys=True)
+
+    # Strips off the leading and closing curly brackets, along with any associated newlines
+    jsn_txt = jsn_txt[1:-1].strip("\n",)
+
+    s = ""
+    s += f">> {sample_name}\n"
+    s += f"{jsn_txt}\n"
+    print(s)
+
+processor_instance = data_comp.AnalysisProcessor(samples_to_process)
+
+if executor == "work_queue":
+    executor_args = {
+        'master_name': '{}-workqueue-coffea'.format(os.environ['USER']),
+
+        # find a port to run work queue in this range:
+        'port': [9123,9130],
+
+        'debug_log': 'debug.log',
+        'transactions_log': 'tr.log',
+        'stats_log': 'stats.log',
+
+        'environment_file': remote_environment.get_environment(),
+        'extra_input_files': ['data_comp.py'],
+
+        # use mid-range compression for chunks results. 9 is the default for work
+        # queue in coffea. Valid values are 0 (minimum compression, less memory
+        # usage) to 16 (maximum compression, more memory usage).
+        'compression': 9,
+
+        # automatically find an adequate resource allocation for tasks.
+        # tasks are first tried using the maximum resources seen of previously ran
+        # tasks. on resource exhaustion, they are retried with the maximum resource
+        # values, if specified below. if a maximum is not specified, the task waits
+        # forever until a larger worker connects.
+        'resource_monitor': True,
+        'resources_mode': 'auto',
+
+        # this resource values may be omitted when using
+        # resources_mode: 'auto', but they do make the initial portion
+        # of a workflow run a little bit faster.
+        # Rather than using whole workers in the exploratory mode of
+        # resources_mode: auto, tasks are forever limited to a maximum
+        # of 8GB of mem and disk.
+        #
+        # NOTE: The very first tasks in the exploratory
+        # mode will use the values specified here, so workers need to be at least
+        # this large. If left unspecified, tasks will use whole workers in the
+        # exploratory mode.
+        #'cores': 1,
+        #'disk': 8000,   #MB
+        #'memory': 10000, #MB
+
+        # control the size of accumulation tasks. Results are
+        # accumulated in groups of size chunks_per_accum, keeping at
+        # most chunks_per_accum at the same time in memory per task.
+        'chunks_per_accum': 25,
+        'chunks_accum_in_mem': 2,
+
+        # terminate workers on which tasks have been running longer than average.
+        # This is useful for temporary conditions on worker nodes where a task will
+        # be finish faster is ran in another worker.
+        # the time limit is computed by multipliying the average runtime of tasks
+        # by the value of 'fast_terminate_workers'.  Since some tasks can be
+        # legitimately slow, no task can trigger the termination of workers twice.
+        #
+        # warning: small values (e.g. close to 1) may cause the workflow to misbehave,
+        # as most tasks will be terminated.
+        #
+        # Less than 1 disables it.
+        'fast_terminate_workers': 0,
+
+        # print messages when tasks are submitted, finished, etc.,
+        # together with their resource allocation and usage. If a task
+        # fails, its standard output is also printed, so we can turn
+        # off print_stdout for all tasks.
+        'verbose': True,
+        'print_stdout': False,
+    }
+
+# Run the processor and get the output
+tstart = time.time()
+
+if executor == "futures":
+    exec_instance = processor.FuturesExecutor(workers=8)
+elif executor ==  "work_queue":
+    exec_instance = processor.WorkQueueExecutor(**executor_args)
+else:
+    raise Exception(f"Executor \"{executor}\" is not known.")
+
+runner = processor.Runner(exec_instance, schema=NanoAODSchema, chunksize=chunksize, maxchunks=nchunks, skipbadfiles=False, xrootdtimeout=180)
+output = runner(flist, treename, processor_instance)
+
+dt = time.time() - tstart
+
+# Save the output
+if not os.path.isdir(outpath): os.system(f"mkdir -p {outpath}")
+out_pkl_file = os.path.join(outpath,outname+".pkl.gz")
+print(f"\nSaving output in {out_pkl_file}...")
+with gzip.open(out_pkl_file, "wb") as fout:
+    cloudpickle.dump(output, fout)
+
+print("Done!")
