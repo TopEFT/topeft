192a083fa9370a045cc868861d2204a179eef904
Andrea Trapote <andreatrapote21@gmail.com>
2022-10-06 10:08:06 +0200

    fixing trigger SFs

--- topcoffea/modules/corrections.py
+++ topcoffea/modules/corrections.py
@@ -652,12 +652,15 @@ def GetTriggerSF(year, events, lep0, lep1):
     SF_em=np.where((events.is2l & events.is_em),LoadTriggerSF(year,ch='2l',flav='em')[syst](lep0.pt,lep1.pt),1.0)
     SF_mm=np.where((events.is2l & events.is_mm),LoadTriggerSF(year,ch='2l',flav='mm')[syst](lep0.pt,lep1.pt),1.0)
     #3l
+    '''
     SF_eee=np.where((events.is3l & events.is_eee),LoadTriggerSF(year,ch='3l',flav='eee')[syst](lep0.pt,lep0.eta),1.0)
     SF_eem=np.where((events.is3l & events.is_eem),LoadTriggerSF(year,ch='3l',flav='eem')[syst](lep0.pt,lep0.eta),1.0)
     SF_emm=np.where((events.is3l & events.is_emm),LoadTriggerSF(year,ch='3l',flav='emm')[syst](lep0.pt,lep0.eta),1.0)
     SF_mmm=np.where((events.is3l & events.is_mmm),LoadTriggerSF(year,ch='3l',flav='mmm')[syst](lep0.pt,lep0.eta),1.0)
     ls.append(SF_ee*SF_em*SF_mm*SF_eee*SF_eem*SF_emm*SF_mmm)
+    '''
+    ls.append(SF_ee*SF_em*SF_mm)
   ls[1]=np.where(ls[1]==1.0,0.0,ls[1]) # stat unc. down
   events['trigger_sf']=ls[0] #nominal
-  events['trigger_sfDown']=ls[0]-np.sqrt(ls[1]*ls[1]+0.02*0.02) 
-  events['trigger_sfUp']=ls[0]+np.sqrt(ls[1]*ls[1]+0.02*0.02) 
+  events['trigger_sfDown']=ls[0]-np.sqrt(ls[1]*ls[1]+ls[0]*0.02*ls[0]*0.02)
+  events['trigger_sfUp']=ls[0]+np.sqrt(ls[1]*ls[1]+ls[0]*0.02*ls[0]*0.02) 
