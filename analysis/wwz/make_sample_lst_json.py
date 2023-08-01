1ff72f37b6f455f75d5bef9317edcaeacf8a06f0
Kelci Mohrman <kelci.mohrman@cern.ch>
2023-08-01 14:50:12 -0700

    Fixing sample jsons

--- analysis/wwz/make_sample_lst_json.py
+++ analysis/wwz/make_sample_lst_json.py
@@ -7,28 +7,37 @@ central_UL16APV_sig_dict = {
     "UL16APV_WWZJetsTo4L2Nu" : {
         "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v2_NANOAODSIM_3LepTau_4Lep",
         "histAxisName": "UL16APV_WWZJetsTo4L2Nu",
-        "xsecName": "WWZ",
+        "xsecName": "WWZ4l",
     },
 }
 central_UL16_sig_dict = {
     "UL16_WWZJetsTo4L2Nu" : {
         "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v2_NANOAODSIM_3LepTau_4Lep",
         "histAxisName": "UL16_WWZJetsTo4L2Nu",
-        "xsecName": "WWZ",
+        "xsecName": "WWZ4l",
     },
 }
 central_UL17_sig_dict = {
     "UL17_WWZJetsTo4L2Nu" : {
         "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2_NANOAODSIM_3LepTau_4Lep",
         "histAxisName": "UL17_WWZJetsTo4L2Nu",
-        "xsecName": "WWZ",
+        "xsecName": "WWZ4l",
     },
 }
 central_UL18_sig_dict = {
     "UL18_WWZJetsTo4L2Nu" : {
         "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2_NANOAODSIM_3LepTau_4Lep",
         "histAxisName": "UL18_WWZJetsTo4L2Nu",
-        "xsecName": "WWZ",
+        "xsecName": "WWZ4l",
+    },
+}
+
+# Test dict
+test_wwz_dict = {
+    "UL17_WWZJetsTo4L2Nu" : {
+        "path" : "/store/user/kmohrman/samples/from_keegan_skims_3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2_NANOAODSIM_3LepTau_4Lep",
+        "histAxisName": "UL17_WWZJetsTo4L2Nu",
+        "xsecName": "WWZ4l",
     },
 }
 
@@ -42,6 +51,8 @@ def main():
     sjt.make_jsons_for_dict_of_samples(central_UL16_sig_dict, "/ceph/cms/","2016", out_dir_sig,on_das=False)
     sjt.make_jsons_for_dict_of_samples(central_UL17_sig_dict, "/ceph/cms/","2017", out_dir_sig,on_das=False)
     sjt.make_jsons_for_dict_of_samples(central_UL18_sig_dict, "/ceph/cms/","2018", out_dir_sig,on_das=False)
+
+    sjt.make_jsons_for_dict_of_samples(test_wwz_dict, "/ceph/cms/","2017",".",on_das=False) # An example
 
 
 if __name__ == "__main__":
