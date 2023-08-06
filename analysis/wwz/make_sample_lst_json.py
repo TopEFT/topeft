a8435c07ea4042e1825b07efe4872e752652535c
Kelci <k.mohrman@ufl.edu>
2023-08-05 22:06:59 -0400

    Adding zh samples to json maker

--- analysis/wwz/make_sample_lst_json.py
+++ analysis/wwz/make_sample_lst_json.py
@@ -2,35 +2,64 @@ import os
 from topcoffea.modules.paths import topcoffea_path
 import topcoffea.modules.sample_lst_jsons_tools as sjt
 
-# WWZ signal samples
+############################ Signal samples ############################
+
 central_UL16APV_sig_dict = {
-    "UL16APV_WWZJetsTo4L2Nu" : {
-        "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v2_NANOAODSIM_3LepTau_4Lep",
-        "histAxisName": "UL16APV_WWZJetsTo4L2Nu",
-        "xsecName": "WWZ4l",
+    #"UL16APV_WWZJetsTo4L2Nu" : {
+    #    "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v2_NANOAODSIM_3LepTau_4Lep",
+    #    "histAxisName": "UL16APV_WWZJetsTo4L2Nu",
+    #    "xsecName": "WWZ4l",
+    #},
+    "UL16APV_GluGluZH" : {
+        "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/GluGluZH_HToWWTo2L2Nu_M-125_TuneCP5_13TeV-powheg-pythia8_RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v1_NANOAODSIM_3LepTau_4Lep",
+        "histAxisName": "UL16APV_GluGluZH",
+        "xsecName": "ZH",
     },
 }
+
 central_UL16_sig_dict = {
-    "UL16_WWZJetsTo4L2Nu" : {
-        "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v2_NANOAODSIM_3LepTau_4Lep",
-        "histAxisName": "UL16_WWZJetsTo4L2Nu",
-        "xsecName": "WWZ4l",
+    #"UL16_WWZJetsTo4L2Nu" : {
+    #    "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v2_NANOAODSIM_3LepTau_4Lep",
+    #    "histAxisName": "UL16_WWZJetsTo4L2Nu",
+    #    "xsecName": "WWZ4l",
+    #},
+    "UL16_GluGluZH" : {
+        "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/GluGluZH_HToWWTo2L2Nu_M-125_TuneCP5_13TeV-powheg-pythia8_RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v2_NANOAODSIM_3LepTau_4Lep",
+        "histAxisName": "UL16_GluGluZH",
+        "xsecName": "ZH",
     },
 }
+
 central_UL17_sig_dict = {
-    "UL17_WWZJetsTo4L2Nu" : {
-        "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2_NANOAODSIM_3LepTau_4Lep",
-        "histAxisName": "UL17_WWZJetsTo4L2Nu",
-        "xsecName": "WWZ4l",
+    #"UL17_WWZJetsTo4L2Nu" : {
+    #    "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2_NANOAODSIM_3LepTau_4Lep",
+    #    "histAxisName": "UL17_WWZJetsTo4L2Nu",
+    #    "xsecName": "WWZ4l",
+    #},
+    "UL17_GluGluZH" : {
+        "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/GluGluZH_HToWWTo2L2Nu_M-125_TuneCP5_13TeV-powheg-pythia8_RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2_NANOAODSIM_3LepTau_4Lep",
+        "histAxisName": "UL17_GluGluZH",
+        "xsecName": "ZH",
     },
 }
+
 central_UL18_sig_dict = {
-    "UL18_WWZJetsTo4L2Nu" : {
-        "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2_NANOAODSIM_3LepTau_4Lep",
-        "histAxisName": "UL18_WWZJetsTo4L2Nu",
-        "xsecName": "WWZ4l",
+    #"UL18_WWZJetsTo4L2Nu" : {
+    #    "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/WWZJetsTo4L2Nu_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2_NANOAODSIM_3LepTau_4Lep",
+    #    "histAxisName": "UL18_WWZJetsTo4L2Nu",
+    #    "xsecName": "WWZ4l",
+    #},
+    "UL18_GluGluZH" : {
+        "path" : "/store/user/kdownham/skimOutput/3LepTau_4Lep/GluGluZH_HToWWTo2L2Nu_M-125_TuneCP5_13TeV-powheg-pythia8_RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1",
+        "histAxisName": "UL18_GluGluZH",
+        "xsecName": "ZH",
     },
 }
+
+
+
+
+############################ Test example samples ############################
 
 # Test dict
 test_wwz_dict = {
@@ -41,6 +70,8 @@ test_wwz_dict = {
     },
 }
 
+
+############################ Main ############################
 
 # Uncomment the make_jsons_for_dict_of_samples() lines for the jsons you want to make/remake
 def main():
