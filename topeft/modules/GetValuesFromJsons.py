061be0838f7ce13a35f2874cf8d24b453ff72472
Kelci Mohrman <kmohrman@nd.edu>
2023-08-21 15:04:04 -0400

    Move get general params to topcoffea

--- topeft/modules/GetValuesFromJsons.py
+++ topeft/modules/GetValuesFromJsons.py
@@ -1,24 +1,6 @@
 import json
 from topcoffea.modules.paths import topcoffea_path
 
-# Return the lumi from the json/lumi.json file for a given year
-def get_lumi(year):
-    lumi_json = topcoffea_path("json/lumi.json")
-    with open(lumi_json) as f_lumi:
-        lumi = json.load(f_lumi)
-        lumi = lumi[year]
-    return lumi
-
-
-# Retrun the param value from params.json for a given param name
-def get_param(param_name):
-    param_json = topcoffea_path("json/params.json")
-    with open(param_json) as f_params:
-        params = json.load(f_params)
-        param_val = params[param_name]
-    return param_val
-
-
 # Get the systematic value from the rate_systs json
 #   - If literal is True, return the literal string, e.g. "0.88/1.13"
 #   - If literal is False, return a pair of floats e.g. [0.88,1.13] for down and up
