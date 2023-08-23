05e817a8d16bebb7cfe5154cd8d45348f8e01f5b
Kelci Mohrman <kmohrman@nd.edu>
2023-08-22 22:48:53 -0400

    Update analysis dir path for ci

--- tests/test_futures.py
+++ tests/test_futures.py
@@ -7,14 +7,14 @@ def test_topcoffea():
     args = [
         "time",
         "python",
-        "analysis/topEFT/run_topeft.py",
+        "analysis/topeft_run2/run_topeft.py",
         "-x",
         "futures",
         "input_samples/sample_jsons/test_samples/UL17_private_ttH_for_CI.json",
         "-o",
         "output_check_yields",
         "-p",
-        "analysis/topEFT/histos/"
+        "analysis/topeft_run2/histos/"
     ]
 
     # Run TopCoffea
@@ -27,7 +27,7 @@ def test_nonprompt():
     a=dataDrivenEstimation.DataDrivenProducer('analysis/topeft_run2/histos/output_check_yields.pkl.gz', 'analysis/topeft_run2/histos/output_check_yields_nonprompt')
     a.dumpToPickle() # Do we want to write this file when testing in CI? Maybe if we ever save the CI artifacts
 
-    assert (exists('analysis/topEFT/histos/output_check_yields_nonprompt.pkl.gz'))
+    assert (exists('analysis/topeft_run2/histos/output_check_yields_nonprompt.pkl.gz'))
 
 def test_datacardmaker():
     args = [
@@ -48,4 +48,4 @@ def test_datacardmaker():
     # Run datacard maker
     subprocess.run(args, check=True)
 
-    assert (comp_datacard('histos/ttx_multileptons-2lss_p_4j_lj0pt.txt','analysis/topEFT/test/ttx_multileptons-2lss_p_4j_lj0pt.txt'))
+    assert (comp_datacard('histos/ttx_multileptons-2lss_p_4j_lj0pt.txt','analysis/topeft_run2/test/ttx_multileptons-2lss_p_4j_lj0pt.txt'))
