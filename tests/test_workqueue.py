8c8182e6eddf566f876edf067172c92dad6b0851
Kelci Mohrman <kmohrman@nd.edu>
2023-08-23 23:29:32 -0400

    Fix run script name

--- tests/test_workqueue.py
+++ tests/test_workqueue.py
@@ -15,7 +15,7 @@ def test_topcoffea_wq():
     args = [
         "time",
         "python",
-        "run_topeft.py",
+        "run_analysis.py",
         "input_samples/sample_jsons/test_samples/UL17_private_ttH_for_CI.json",
         "-o",
         "output_check_yields_wq",
