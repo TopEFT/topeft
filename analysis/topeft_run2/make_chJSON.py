4a6999ce178d73acf4c3fb6b76c7f318c855fe52
Yuyi Wan <ywan2@glados.crc.nd.edu>
2024-04-01 10:49:49 -0400

    add control region and jet info in make_chJSON.py

--- analysis/topeft_run2/make_chJSON.py
+++ analysis/topeft_run2/make_chJSON.py
@@ -2,34 +2,35 @@ import json
 
 # Define the dictionary
 data = {
-    "TOP22_006_CH_LST": {
+    "TOP22_006_CH_LST_SR": {
         "2l": {
             "lep_chan_lst": ["2lss_p", "2lss_m", "2lss_4t_p", "2lss_4t_m"],
             "lep_flav_lst": ["ee", "em", "mm"],
             "appl_lst": ["isSR_2lSS", "isAR_2lSS"],
             "appl_lst_data": ["isAR_2lSS_OS"],
-            "jet_lst": [4, 5, 6, 7]
+            "jet_lst": ["exactly_4j", "exactly_5j", "exactly_6j", "atleast_7j"]
         },
         "3l": {
             "lep_chan_lst": ["3l_p_offZ_1b", "3l_m_offZ_1b", "3l_p_offZ_2b", "3l_m_offZ_2b", "3l_onZ_1b", "3l_onZ_2b"],
             "lep_flav_lst": ["eee", "eem", "emm", "mmm"],
             "appl_lst": ["isSR_3l", "isAR_3l"],
-            "jet_lst": [2, 3, 4, 5]
+            "jet_lst": ["exactly_2j", "exactly_3j", "exactly_4j", "atleast_5j"]
         },
         "4l": {
             "lep_chan_lst": ["4l"],
             "lep_flav_lst": ["llll"],
             "appl_lst": ["isSR_4l"],
-            "jet_lst": [2, 3, 4]
+            "jet_lst": ["exactly_2j", "exactly_3j", "atleast_4j"]
         }
     },
-    "OFFZ_SPLIT_CH_LST": {
+
+    "OFFZ_SPLIT_CH_LST_SR": {
         "2l": {
             "lep_chan_lst": ["2lss_p", "2lss_m", "2lss_4t_p", "2lss_4t_m"],
             "lep_flav_lst": ["ee", "em", "mm"],
             "appl_lst": ["isSR_2lSS", "isAR_2lSS"],
             "appl_lst_data": ["isAR_2lSS_OS"],
-            "jet_lst": [4, 5, 6, 7]
+            "jet_lst": ["exactly_4j", "exactly_5j", "exactly_6j", "atleast_7j"]
         },
         "3l": {
             "lep_chan_lst": [
@@ -41,13 +42,48 @@ data = {
             ],
             "lep_flav_lst": ["eee", "eem", "emm", "mmm"],
             "appl_lst": ["isSR_3l", "isAR_3l"],
-            "jet_lst": [2, 3, 4, 5]
+            "jet_lst": ["exactly_2j", "exactly_3j", "exactly_4j", "atleast_5j"]
         },
         "4l": {
             "lep_chan_lst": ["4l"],
             "lep_flav_lst": ["llll"],
             "appl_lst": ["isSR_4l"],
-            "jet_lst": [2, 3, 4]
+            "jet_lst": ["exactly_2j", "exactly_3j", "atleast_4j"]
+        }
+    }
+
+    "CH_LST_CR": {
+        "2l_CRflip" : {
+            "lep_chan_lst": ["2lss_CRflip"],
+            "lep_flav_lst": ["ee"],
+            "appl_lst": ["isSR_2lSS" , "isAR_2lSS"],
+            "appl_lst_data": ["isAR_2lSS_OS"],
+            "jet_lst": ["atmost_3j"]
+        },
+        "2l_CR" : {
+            "lep_chan_lst": ["2lss_CR"],
+            "lep_flav_lst": ["ee" , "em" , "mm"],
+            "appl_lst": ["isSR_2lSS" , "isAR_2lSS"],
+            "appl_lst_data": ["isAR_2lSS_OS"],
+            "jet_lst": ["exactly_1j", "exactly_2j", "exactly_3j"]
+        },
+        "3l_CR" : {
+            "lep_chan_lst": ["3l_CR"],
+            "lep_flav_lst": ["eee" , "eem" , "emm", "mmm"],
+            "appl_lst": ["isSR_3l" , "isAR_3l"],
+            "jet_lst": ["exactly_0j", "atleast_1j"]
+        },
+        "2los_CRtt" : {
+            "lep_chan_lst": ["2los_CRtt"],
+            "lep_flav_lst": ["em"],
+            "appl_lst": ["isSR_2lOS" , "isAR_2lOS"],
+            "jet_lst": ["exactly_2j"]
+        },
+        "2los_CRZ" : {
+            "lep_chan_lst": ["2los_CRZ"],
+            "lep_flav_lst": ["ee", "mm"],
+            "appl_lst": ["isSR_2lOS" , "isAR_2lOS"],
+            "jet_lst": ["atleast_0j"]
         }
     }
 }
