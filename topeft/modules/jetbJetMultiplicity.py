d754dec2a6e7b04ebb854b6ef3ab9d6d68cec97e
abasnet97 <basnetaashwin1166@gmail.com>
2024-06-20 16:49:10 -0400

    added a helper function to make jet_bjet plot

--- topeft/modules/jetbJetMultiplicity.py
+++ topeft/modules/jetbJetMultiplicity.py
@@ -1,18 +1,19 @@
-#Relevant for Zgamma CR studies
-#The function returns integer from 0 to 5 for each event. If for a given event, the integer is 4, it means that event has exactly 2 jets and 2 bjets, etc.
+#Relevant only for Zgamma CR studies where we have 5 njet_bjet categories from Top-21-004
+#For completeness, there are other low jet_bjet categories as well
+#The function returns integer from 1 to 9 for each event. If for a given event, the integer is 8, it means that event has exactly 2 jets and 2 bjets, etc.
 
 import awkward as ak
 
-def multiplicityOfJetsAndbJets(jet_collection, bjet_collection):
-    num_jets = ak.num(jet_collection)
-    num_bjets = ak.num(bjet_collection)
-    
+def multiplicityOfJetsAndbJets(num_jets, num_bjets):
+    jets0_bjets0 = ((num_jets==0) & (num_bjets==0))
+    jets1_bjets0 = ((num_jets==1) & (num_bjets==0))
+    jets2_bjets0 = ((num_jets==2) & (num_bjets==0))
+    jetsgeq3_bjets0 = ((num_jets>=3) & (num_bjets==0))
     jets1_bjets1 = ((num_jets==1) & (num_bjets==1))
     jets2_bjets1 = ((num_jets==2) & (num_bjets==1))
     jetsgeq3_bjets1 = ((num_jets>=3) & (num_bjets==1))
     jets2_bjets2 = ((num_jets==2) & (num_bjets==2))
     jetsgeq3_bjets2 = ((num_jets>=3) & (num_bjets==2))
 
-
-    return 1*jets1_bjets1 + 2*jets2_bjets1 + 3*jetsgeq3_bjets1 + 4*jets2_bjets2 + 5*jetsgeq3_bjets2
+    return 1*jets0_bjets0 + 2*jets1_bjets0 + 3*jets2_bjets0 + 4*jetsgeq3_bjets0 + 5*jets1_bjets1 + 6*jets2_bjets1 + 7*jetsgeq3_bjets1 + 8*jets2_bjets2 + 9*jetsgeq3_bjets2
     
