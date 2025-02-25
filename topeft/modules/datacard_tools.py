87a465f2867396890cc17a6f38e26bc7bde6c444
Brent Yates <yates.313@osu.edu>
2025-02-25 13:22:52 -0500

    Handle SMEFTsim topU3l <-> dim6top rotations

--- topeft/modules/datacard_tools.py
+++ topeft/modules/datacard_tools.py
@@ -7,6 +7,7 @@ import hist
 import os
 import re
 import json
+import yaml 
 import time
 
 from collections import defaultdict
@@ -305,6 +306,26 @@ class DatacardMaker():
         # get wc ranges from json
         with open(topeft_path("params/wc_ranges.json"), "r") as wc_ranges_json:
             self.wc_ranges = json.load(wc_ranges_json)
+
+        with open(topeft_path("params/SMEFTsim-topU3l_dim6top.yml")) as f:
+            self.rotate = yaml.load(f,Loader=yaml.CLoader)
+            for k,v in self.rotate.items():
+                if isinstance(v, str):
+                    if "CW" in v or "SW" in v:
+                        v = eval(v.replace("{CW}", str(self.rotate["CW"])).replace("{SW}", str(self.rotate["SW"])))
+                        self.rotate[k] = v
+                elif isinstance(v, list):
+                    for i,vv in enumerate(v):
+                        if "CW" in vv or "SW" in vv:
+                            v[i] = vv.replace("{CW}", str(self.rotate["CW"])).replace("{SW}", str(self.rotate["SW"]))
+                    wcs = v[1]
+                    v   = v[0]
+                    wcs = {wc: wc+"[0, " + str(self.wc_ranges[wc][0]) + ", " + str(self.wc_ranges[wc][1]) + "]" for wc in wcs}
+                    v = v.format(**wcs)
+                    self.rotate[k] = v
+                if isinstance(v, str) and v in self.wc_ranges:
+                    v = v.replace(v, v+"[0, " + str(self.wc_ranges[v][0]) + ", " + str(self.wc_ranges[v][1]) + "]")
+                    self.rotate[k] = v
 
         if self.year_lst:
             for yr in self.year_lst:
@@ -788,6 +809,8 @@ class DatacardMaker():
         return scalings_json
 
     def format_wc(self,wcname):
+        if wcname in self.rotate:
+            return self.rotate[wcname]
         lo, hi = self.wc_ranges[wcname]
         return "%s[0,%.1f,%.1f]" % (wcname, lo, hi)
 
