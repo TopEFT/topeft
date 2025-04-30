1afe914eb9d4bf3bbfe50c687028a262b39c486f
abasnet97 <basnetaashwin1166@gmail.com>
2025-04-30 16:06:55 -0400

    update new photon FR and kMC npz files and ttgamma correction factor file

---
+++ topeft/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name='topeft',
+    version='0.0.0',
+    description='Analysis code for top quark EFT analyses',
+    packages=setuptools.find_packages(),
+    # Include data files (Note: "include_package_data=True" does not seem to work)
+    package_data={
+        "topeft" : [
+            "channels/*",
+            "params/*",
+            "data/fliprates/*.pkl.gz",
+            "data/triggerSF/*.pkl.gz",
+            "data/btagSF/UL/*.pkl.gz",
+            "data/btagSF/UL/*.csv",
+            "modules/*.json",
+            "data/photon_fakerates_*/*.npz",
+            "data/photon_kmc_*/*.npz",
+            "data/ttg_private_corrections/cf.json"
+        ],
+    }
+)
+
