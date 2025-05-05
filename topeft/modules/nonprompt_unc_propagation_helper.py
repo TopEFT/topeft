b325ab118f1014a8d6304d4b15e8eb75c933244a
abasnet97 <basnetaashwin1166@gmail.com>
2025-05-05 14:19:00 -0400

    few more changes on non prompt photon estimation block

--- topeft/modules/nonprompt_unc_propagation_helper.py
+++ topeft/modules/nonprompt_unc_propagation_helper.py
@@ -1,6 +1,6 @@
 #Where is it used?: This script is used in dataDrivenEstimation script.
 #What is the purpose?: The main purpose of this is to correctly handle the non-prompt photon MC stat uncertainty in the photon_pt histogram. This script makes sure that the FR and kMC uncertainties are included in the photon_pt histograms
-#What does it take?: It takes the photon_pt_eta and photon_pt_eta_sumw2 histograms and uses it to modify the sumw2 values stored in the photon_pt2_sumw2 histogram
+#What does it take?: It takes the photon_pt_eta and photon_pt_eta_sumw2 histograms and uses it to modify the sumw2 values stored in the photon_pt_sumw2 histogram
 #Future modifications: Potential places with pitfalls in the future are commented with "CAUTION" tag
 
 import re
@@ -30,17 +30,14 @@ def modify_NP_photon_pt_eta_variance(dict_of_hists_for_NP_uncertainty, closure=F
             if match: year = match.group(1)
 
         #We need to load the fake-rate and kMC files inside cause they depend on year!
-        #fr_file_path = topeft_path("data/photon_fakerates_gyR6uGhvfy/")+f"fr_ph_UL{year}.npz"
-        fr_file_path = topeft_path("data/photon_fakerates_o7cANdcK3j/")+f"fr_ph_UL{year}.npz"
+        fr_file_path = topeft_path("data/photon_fakerates_ZPVR6zQL2M/")+f"fr_ph_UL{year}.npz"
 
         #Depending on whether we are doing closure test or not, the kMC file changes
         if closure:
-            #kmc_file_path = topeft_path("data/photon_fakerates_jeJHI2cDh5/")+f"kmc_ph_UL{year}.npz"
-            kmc_file_path = topeft_path("data/photon_fakerates_5QFHsLhmPF/")+f"kmc_ph_UL{year}.npz"
+            kmc_file_path  = topeft_path("data/photon_kmc_closure_z04kenfTjD/")+f"kmc_ph_closure_UL{year}.npz"
 
         else:
-            #kmc_file_path = topeft_path("data/photon_fakerates_gB29WFMqFb/")+f"kmc_ph_UL{year}.npz"
-            kmc_file_path = topeft_path("data/photon_fakerates_ZxY8lNRB2E/")+f"kmc_ph_UL{year}.npz"
+            kmc_file_path  = topeft_path("data/photon_kmc_L2G8oDYJv8/")+f"kmc_ph_UL{year}.npz"
 
         #Load the FR and kMC files
         ph_fr_val, ph_fr_err = load_numpy_files(fr_file_path)
@@ -63,20 +60,20 @@ def modify_NP_photon_pt_eta_variance(dict_of_hists_for_NP_uncertainty, closure=F
             sumw_pt_eta_hist[:] = ph_ff_val*sumw_pt_eta_hist
 
 
-#This histogram takes the output histogram dictionary called "outHist" in the dataDrivenEstimation script, where we will have a key called "photon_pt2_sumw2". This histogram will also have the non-prompt estimation done, so it will have a key called "nonPromptPhUL<year>", and we need to modify the values associated with that key. The photon_pt2_sumw2 histogram has same binning as FR/kMC
+#This histogram takes the output histogram dictionary called "outHist" in the dataDrivenEstimation script, where we will have a key called "photon_pt_sumw2". This histogram will also have the non-prompt estimation done, so it will have a key called "nonPromptPhUL<year>", and we need to modify the values associated with that key. The photon_pt_sumw2 histogram has same binning as FR/kMC
 #Also note that at this point, the photon_pt_eta_sumw2 histogram will already have the values modified as described above
 #CAUTION: Revisit this in this future cause we will just have one photon_pt histogram and the name will need to be changed accordingly
 def modify_photon_pt_variance(outputHist,year):
-    photon_pt2_sumw2_hist = outputHist['photon_pt2_sumw2']
-    for k in list(outputHist['photon_pt2_sumw2'].eval({}).keys()):
+    photon_pt_sumw2_hist = outputHist['photon_pt_sumw2']
+    for k in list(outputHist['photon_pt_sumw2'].eval({}).keys()):
         if any(str(part) == f"nonpromptPhUL{year}" for part in k):
-            #First, load the values of photon_pt2_sumw2 hist
-            photon_pt2_sumw2 = photon_pt2_sumw2_hist.eval({})[k]
+            #First, load the values of photon_pt_sumw2 hist
+            photon_pt_sumw2 = photon_pt_sumw2_hist.eval({})[k]
             #Second, load the modified values from photon_pt_eta_sumw2 hist
             np_2D_ph_var = outputHist['photon_pt_eta_sumw2'].view(flow=True)[k]
 
             #WE first have to add over eta bins to get uncertainties associated with the pt bins
             new_sumw2_summed_over_eta = np.sum(np_2D_ph_var,1)  #At this point this array has a shape of 1 X (N_photon_pt_bins+1)
 
-            #We are now ready to modify the values of the photon_pt2_sumw2 histogram
-            photon_pt2_sumw2_hist[k] = new_sumw2_summed_over_eta[:,np.newaxis]
+            #We are now ready to modify the values of the photon_pt_sumw2 histogram
+            photon_pt_sumw2_hist[k] = new_sumw2_summed_over_eta[:,np.newaxis]
