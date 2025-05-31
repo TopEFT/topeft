939ee487bb3482dd90c5dce651fa197fb43ccaab
Andrea Piccinelli <andrea8395@gmail.com>
2025-05-31 06:42:28 -0400

    remove unneeded line in paths.py

--- topeft/modules/paths.py
+++ topeft/modules/paths.py
@@ -2,7 +2,7 @@ import os
 import topeft
 
 pjoin = os.path.join
-print(topeft.__path__[0])
+
 # This function takes as input any path (inside of topeft/topeft), and returns the absolute path
 def topeft_path(path_in_repo):
     return pjoin(topeft.__path__[0], path_in_repo)
