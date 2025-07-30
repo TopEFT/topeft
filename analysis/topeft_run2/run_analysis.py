1413878b95ff21ceee928706c0f444cc9bb0c359
Andrea Piccinelli <andrea8395@gmail.com>
2025-07-30 04:54:19 -0400

    bjet gen variables

--- analysis/topeft_run2/run_analysis.py
+++ analysis/topeft_run2/run_analysis.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
             hist_lst.append("lt")
     elif args.hist_list == ["cr"]:
         # Here we hardcode a list of hists used for the CRs
-        hist_lst = ["lj0pt", "ptz", "met", "ljptsum", "l0pt", "l0ptcorr", "l0conept", "l0eta", "l1pt", "l1ptcorr", "l1conept", "l1eta", "j0pt", "j0eta", "njets", "nbtagsl", "invmass", "npvs", "npvsGood", "l0_gen_pdgId", "l1_gen_pdgId", "l2_gen_pdgId", "l0_genParent_pdgId", "l1_genParent_pdgId", "l2_genParent_pdgId"]
+        hist_lst = ["lj0pt", "ptz", "met", "ljptsum", "l0pt", "l0ptcorr", "l0conept", "l0eta", "l1pt", "l1ptcorr", "l1conept", "l1eta", "j0pt", "j0eta", "njets", "nbtagsl", "invmass", "npvs", "npvsGood", "l0_gen_pdgId", "l1_gen_pdgId", "l2_gen_pdgId", "l0_genParent_pdgId", "l1_genParent_pdgId", "l2_genParent_pdgId", "b0l_hFlav", "b0m_hFlav", "b0l_pFlav", "b0m_pFlav", "b1l_hFlav", "b1m_hFlav", "b1l_pFlav", "b1m_pFlav", "b0l_genhFlav", "b0m_genhFlav", "b0l_genpFlav", "b0m_genpFlav", "b1l_genhFlav", "b1m_genhFlav", "b1l_genpFlav", "b1m_genpFlav"]
         if tau_h_analysis:
             hist_lst.append("tau0pt")
     else:
--- topeft/modules/axes.py
+++ topeft/modules/axes.py
@@ -117,7 +117,8 @@ info = {
         "regular": (12, 0, 600),
         "variable": [0,150,250,500],
         "label": r"Scalar sum of met at leading leptons (GeV)",
-        },
+    },
+
     "l0_gen_pdgId": {
        "regular": (27, -0.5, 26.5),
        "label": r"pdgid of l0 genparticle",
@@ -141,5 +142,70 @@ info = {
     "l2_genParent_pdgId": {
        "regular": (27, -0.5, 26.5),
        "label": r"pdgid of l2 genparent",
-    }
+    },
+
+    "b0l_hFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"Hadron Flavor of leading loose b jet",
+   },
+    "b0l_pFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"Parton Flavor of leading loose b jet",
+   },
+    "b0m_hFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"Hadron Flavor of leading medium b jet",
+   },
+    "b0m_pFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"Parton Flavor of leading medium b jet",
+   },
+    "b1l_hFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"Hadron Flavor of subleading loose b jet",
+   },
+    "b1l_pFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"Parton Flavor of subleading loose b jet",
+   },
+    "b1m_hFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"Hadron Flavor of subleading medium b jet",
+   },
+    "b1m_pFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"Parton Flavor of subleading medium b jet",
+   },
+    "b0l_genhFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"GenHadron Flavor of leading loose b jet",
+   },
+    "b0l_genpFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"GenParton Flavor of leading loose b jet",
+   },
+    "b0m_genhFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"GenHadron Flavor of leading medium b jet",
+   },
+    "b0m_genpFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"GenParton Flavor of leading medium b jet",
+   },
+    "b1l_genhFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"GenHadron Flavor of subleading loose b jet",
+   },
+    "b1l_genpFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"GenParton Flavor of subleading loose b jet",
+   },
+    "b1m_genhFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"GenHadron Flavor of subleading medium b jet",
+   },
+    "b1m_genpFlav": {
+       "regular": (27, -0.5, 26.5),
+       "label": r"GenParton Flavor of subleading medium b jet",
+   }
 }
