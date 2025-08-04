0dd6c2bdb6e355ad5ebe63bbe09cb0ebacbc8a19
Andrea Piccinelli <andrea8395@gmail.com>
2025-08-04 16:41:57 -0400

    possibly fixing an error with tmp

--- analysis/topeft_run2/fullR2_run.sh
+++ analysis/topeft_run2/fullR2_run.sh
@@ -1,16 +1,16 @@
 # This script runs the wq run script with all of the settings appropriate for making SR histos for the full R2 analysis
 
 # Name the output
-OUT_NAME="example_name"
+OUT_NAME="run2taus_250804_merged"
 
 # Build the run command for filling SR histos
 OUT_NAME+="_SRs"
 CFGS="../../input_samples/cfgs/mc_signal_samples_NDSkim.cfg,../../input_samples/cfgs/mc_background_samples_NDSkim.cfg,../../input_samples/cfgs/data_samples_NDSkim.cfg"
-OPTIONS="--hist-list ana --skip-cr --do-systs -s 50000 --do-np -o $OUT_NAME " # For analysis
+OPTIONS="--hist-list ana --skip-cr --do-systs -s 50000 --do-np -o $OUT_NAME --tau_h_analysis" # For analysis
 
 
 # Build the run command for filling CR histos
-OUT_NAME+="_CRs"
+#OUT_NAME+="_CRs"
 #CFGS="../../input_samples/cfgs/mc_signal_samples_NDSkim.cfg,../../input_samples/cfgs/mc_background_samples_NDSkim.cfg,../../input_samples/cfgs/mc_background_samples_cr_NDSkim.cfg,../../input_samples/cfgs/data_samples_NDSkim.cfg"
 #OPTIONS="--hist-list cr --skip-sr --do-systs --do-np --wc-list ctG -o $OUT_NAME --split-lep-flavor" # For CR plots
 
