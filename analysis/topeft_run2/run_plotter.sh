8c4c8fee72803d48ffa2457c729bb34666f29c73
Yuyi Wan <ywan2@glados.crc.nd.edu>
2025-09-28 21:52:41 -0400

    keep current setup

--- analysis/topeft_run2/run_plotter.sh
+++ analysis/topeft_run2/run_plotter.sh
@@ -16,4 +16,4 @@ YEAR="2022"
 TYPE="" #Central"
 COMMIT="d86b067_DeepJet"
 #python make_cr_and_sr_plots.py -f histoR3/2022_run3_object_selection_test.pkl.gz -o /scratch365/apiccine/run3plots -t -y 2022 -s -n mytest22
-python make_cr_and_sr_plots.py -f /scratch365/apiccine/${YEAR}CRs${TYPE}_${COMMIT}_np.pkl.gz -o /scratch365/apiccine/run3plots -t -y $YEAR -s -n test${YEAR}CRs${TYPE}_${COMMIT}
+python make_cr_and_sr_plots.py -f /scratch365/ywan2/${YEAR}CRs_fixTauCut_np.pkl.gz -o histos/CR_plots -t -y $YEAR -s -n ${YEAR}CRs_fixTauCut
