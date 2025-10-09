f6a56fe2ccc9ed50e08294e474511d69f0b1067d
Brent Yates <yates.313@osu.edu>
2025-10-09 15:19:26 -0400

    Change invm range, just H boson for now

--- analysis/mc_validation/gen_processor.py
+++ analysis/mc_validation/gen_processor.py
@@ -73,7 +73,7 @@ class AnalysisProcessor(processor.ProcessorABC):
             "mll_fromzg_m" : HistEFT(proc_axis, chan_axis, syst_axis, appl_axis, hist.axis.Regular(40,  0, 200,  name="mll_fromzg_m", label=r"invmass mm from z/gamma"), wc_names=wc_names_lst, rebin=False),
             "mll_fromzg_t" : HistEFT(proc_axis, chan_axis, syst_axis, appl_axis, hist.axis.Regular(40,  0, 200,  name="mll_fromzg_t", label=r"invmass tautau from z/gamma"), wc_names=wc_names_lst, rebin=False),
             "mll"          : HistEFT(proc_axis, chan_axis, syst_axis, appl_axis, hist.axis.Regular(30,  0, 300,  name="mll",          label=r"Invmass l0l1"), wc_names=wc_names_lst, rebin=False),
-            "invm"          : HistEFT(proc_axis, chan_axis, syst_axis, appl_axis, hist.axis.Regular(100,  0, 1000,  name="invm",        label=r"Invmass of system"), wc_names=wc_names_lst, rebin=False),
+            "invm"          : HistEFT(proc_axis, chan_axis, syst_axis, appl_axis, hist.axis.Regular(100,  1000, 5000,  name="invm",        label=r"Invmass of system"), wc_names=wc_names_lst, rebin=False),
             "ht"           : HistEFT(proc_axis, chan_axis, syst_axis, appl_axis, hist.axis.Regular(100, 0, 1000, name="ht",           label=r"Scalar sum of genjet pt"), wc_names=wc_names_lst, rebin=False),
             "ht_clean"     : HistEFT(proc_axis, chan_axis, syst_axis, appl_axis, hist.axis.Regular(100, 0, 1000, name="ht_clean",     label=r"Scalar sum of clean genjet pt"), wc_names=wc_names_lst, rebin=False),
             "lhe_t_pt"      : HistEFT(proc_axis, chan_axis, syst_axis, appl_axis, hist.axis.Regular(50,  0, 500,   name="lhe_t_pt",     label=r"Pt of the leading LHE t"), wc_names=wc_names_lst, rebin=False),
@@ -171,6 +171,7 @@ class AnalysisProcessor(processor.ProcessorABC):
 
         gen_top = ak.pad_none(genpart[(abs(genpart.pdgId) == 6)],2)
         gen_bos = ak.pad_none(genpart[(abs(genpart.pdgId) == 23) | (abs(genpart.pdgId) == 24) | abs(genpart.pdgId) == 25],2)
+        gen_bos = ak.pad_none(genpart[abs(genpart.pdgId) == 25],2)
         #dilep_mask = (np.abs(lhepart.pdgId)) == 13 | (np.abs(lhepart.pdgId) == 11)
         #nu_mask = (np.abs(lhepart.pdgId)) == 14 | (np.abs(lhepart.pdgId) == 12)
         #b_mask = (np.abs(lhepart.pdgId)) == 5
