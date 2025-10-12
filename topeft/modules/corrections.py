c93a855e5bcc26fb86c3c24d745853aba8c6c476
Andrea Piccinelli <44297061+anpicci@users.noreply.github.com>
2025-10-13 00:37:24 +0200

    Use per-tau gen flavour in Run3 FES systematics

--- topeft/modules/corrections.py
+++ topeft/modules/corrections.py
@@ -810,7 +810,7 @@ def ApplyFESSystematic(year, taus, isData, syst_name, vsJetWP="Loose"):
                 ak.to_numpy((flat_pt[whereFlag])),
                 ak.to_numpy((flat_eta[whereFlag])),
                 ak.to_numpy((flat_dm[whereFlag])),
-                1,
+                ak.to_numpy((flat_gen[whereFlag])),
                 "DeepTau2018v2p5",
                 vsJetWP,
                 "VVLoose",
