0f605ef41f45696f2b454f3be57ef55b0da56be8
Andrea Piccinelli <andrea8395@gmail.com>
2025-10-12 12:42:29 -0400

    minor fixes

--- analysis/topeft_run2/fullR2_run.sh
+++ analysis/topeft_run2/fullR2_run.sh
@@ -4,7 +4,7 @@
 OUT_NAME="refact_250901_c1"
 
 # Build the run command for filling SR histos
-OUT_NAME+="_SRs"
+#OUT_NAME+="_SRs"
 # CFGS="../../input_samples/cfgs/mc_signal_samples_NDSkim.cfg.../../input_samples/cfgs/mc_background_samples_NDSkim.cfg,../../input_samples/cfgs/data_samples_NDSkim.cfg"
 # CFGS="../../input_samples/cfgs/data_samples_NDSkim.cfg"
 # OPTIONS="--skip-cr --do-systs -o $OUT_NAME -c 1 -s 50 -x futures " #--split-lep-flavor " # Add "--scenario tau_analysis" or "--channel-feature requires_tau" for tau studies
@@ -13,7 +13,7 @@ OUT_NAME+="_SRs"
 
 
 # Build the run command for filling CR histos
-# OUT_NAME+="_CRs"
+OUT_NAME+="_CRs"
 # CFGS="../../input_samples/cfgs/mc_signal_samples_NDSkim.cfg,../../input_samples/cfgs/mc_background_samples_NDSkim.cfg,"
 CFGS="../../input_samples/cfgs/mc_background_samples_cr_NDSkim.cfg"
 # CFGS="../../input_samples/cfgs/data_samples_NDSkim.cfg"
