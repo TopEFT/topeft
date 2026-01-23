20b441da88b2c811c5c5324fb8edff0425293ac2
Andrea Piccinelli <andrea8395@gmail.com>
2026-01-23 09:42:43 -0500

    tweaking accumulation parameters

--- analysis/topeft_run2/run_analysis.py
+++ analysis/topeft_run2/run_analysis.py
@@ -170,7 +170,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--chunksize",
         "-s",
-        default=50000,
+        default=20000,
         help="Number of events per chunk",
     )
     parser.add_argument(
