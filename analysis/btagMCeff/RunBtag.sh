7ea53bdd519928b3b4182810a8537e40ce641489
Yuyi Wan <ywan2@nd.edu>
2026-03-23 16:47:01 -0400

    edit RunBtag script to process only central signal

--- analysis/btagMCeff/RunBtag.sh
+++ analysis/btagMCeff/RunBtag.sh
@@ -1,11 +1,14 @@
+'''
+Both Run2 and Run3 btag eff maps are made with ttH central samples, make sure to select the right json in the cfg files.
+'''
+
 # Name the output
  
 OUT_NAME="2022_btag_efficiency_map_srbg_DeepJet"
-CFGS="../../input_samples/cfgs/ND_2022_signal_samples.cfg,../../input_samples/cfgs/ND_2022_background_samples.cfg"
-#CFGS="../../input_samples/cfgs/ND_2023_signal_samples.cfg,../../input_samples/cfgs/ND_2023_background_samples.cfg"
+CFGS="../../input_samples/cfgs/ND_2022_signal_samples.cfg"
 #CFGS="../../input_samples/cfgs/ND_2022EE_signal_samples.cfg"
-#CFGS="../../input_samples/cfgs/ND_2022EE_signal_samples.cfg,../../input_samples/cfgs/ND_2022EE_background_samples.cfg"
-#CFGS="../../input_samples/cfgs/ND_2023BPix_signal_samples.cfg,../../input_samples/cfgs/ND_2023BPix_background_samples.cfg"
+#CFGS="../../input_samples/cfgs/ND_2023_signal_samples.cfg"
+#CFGS="../../input_samples/cfgs/ND_2023BPix_signal_samples.cfg"
 
 OPTIONS=" -o $OUT_NAME" # add --test to run over a few set of events                                               
 RUN_COMMAND="time python run.py $CFGS $OPTIONS"
